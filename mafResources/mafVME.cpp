e805c73d8a9a4889e6fe57d0244ee10fbb835172
elpablo <paolo.quadrani@gmail.com>
2010-10-07 18:26:47 +0200

    Fixed the implementation of setDataPipe to accept also NUL data pipe that simply will allow to delete the old one.

--- mafResources/mafVME.cpp
+++ mafResources/mafVME.cpp
@@ -13,35 +13,58 @@
 #include "mafDataPipe.h"
 #include "mafDataSet.h"
 #include "mafMementoDataSet.h"
+#include "mafInteractor.h"
 
 using namespace mafCore;
 using namespace mafResources;
 
-mafVME::mafVME(const mafString code_location) : mafResource(code_location), m_DataSetCollection(NULL), m_DataPipe(NULL) {
+mafVME::mafVME(const mafString code_location) : mafResource(code_location), m_Modified(false), m_Interactor(NULL), m_DataSetCollection(NULL), m_DataPipe(NULL) {
     mafId time_set_id = mafIdProvider::instance()->idValue("TIME_SET");
     if(time_set_id != -1) {
         mafRegisterLocalCallback("TIME_SET", this, "setTimestamp(double)");
     }
-    m_MementoDatSetHash.clear();
+    m_MementoDataSetHash.clear();
+    connect(this, SIGNAL(modifiedObject()), this, SLOT(execute()));
 }
 
 mafVME::~mafVME() {
+    mafDEL(m_Interactor);
     mafDEL(m_DataSetCollection);
     mafDEL(m_DataPipe);
-    mafHash<mafMementoDataSet *, double>::iterator iter = m_MementoDatSetHash.begin();
-    while(iter != m_MementoDatSetHash.end()) {
+
+    // delete all the created mementos for the dataset collection.
+    mafHash<mafMementoDataSet *, double>::iterator iter = m_MementoDataSetHash.begin();
+    while(iter != m_MementoDataSetHash.end()) {
         mafMementoDataSet *memento = iter.key();
         mafDEL(memento);
         iter++;
     }
-    m_MementoDatSetHash.clear();
+    m_MementoDataSetHash.clear();
+}
+
+void mafVME::setModified(bool m) {
+    m_Modified = m;
+
+    if(!m_Modified) {
+        return;
+    }
+
+    // Connect this modifiedObject signal with the class that hsould listen it
+    // like the mafDataPipe or mafVisualPipe
+    emit(modifiedObject());
 }
 
 void mafVME::setTimestamp(double t) {
     dataSetCollection()->setTimestamp(t);
+    setModified();
+}
 
-    // VME is now modified, should ask for an update.
-    execute();
+void mafVME::setInteractor(mafInteractor *i) {
+    if(i == m_Interactor) {
+        return;
+    }
+    mafDEL(m_Interactor);
+    m_Interactor = i;
 }
 
 void mafVME::setDataPipe(const mafString &pipe_type) {
@@ -49,23 +72,23 @@ void mafVME::setDataPipe(const mafString &pipe_type) {
     mafDataPipe *new_pipe = dynamic_cast<mafDataPipe *>(obj);
     if(new_pipe) {
         setDataPipe(new_pipe);
+        return;
     } else {
         mafMsgWarning("%s", mafTr("%1 does not represent a type of mafDataPipe.").arg(pipe_type).toAscii().data());
     }
+    mafDEL(obj);
 }
 
 void mafVME::setDataPipe(mafDataPipe *pipe) {
-    REQUIRE(pipe != NULL);
-
     mafDEL(m_DataPipe);
     m_DataPipe = pipe;
+    setModified();
 }
 
-bool mafVME::execute() {
+void mafVME::execute() {
     if(m_DataPipe) {
         m_DataPipe->updatePipe(dataSetCollection()->timeStamp());
     }
-    return true;
 }
 
 mafDataSet *mafVME::outputData() {
@@ -116,17 +139,18 @@ void mafVME::setMemento(mafMemento *memento, bool deep_memento) {
                 i++;
                 propList->append(list->at(i));
             }
-            m_MementoDatSetHash.insert(mementoDataSet, time);
+            m_MementoDataSetHash.insert(mementoDataSet, time);
 
         } else if(item.m_Name == "mafDataPipe") {
             this->setDataPipe(item.m_Value.toString());
         }
     }
+    setModified();
 }
 
 void mafVME::updateData() {
-    mafHash<mafMementoDataSet *, double>::iterator iter = m_MementoDatSetHash.begin();
-    while(iter != m_MementoDatSetHash.end()) {
+    mafHash<mafMementoDataSet *, double>::iterator iter = m_MementoDataSetHash.begin();
+    while(iter != m_MementoDataSetHash.end()) {
         mafDataSet *dataSet = mafNEW(mafResources::mafDataSet);
         mafMementoDataSet *memento = iter.key();
         memento->setObjectClassType(dataSet->metaObject()->className());
