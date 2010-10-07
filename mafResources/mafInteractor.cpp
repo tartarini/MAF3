ed494b68d1675a14d032e4c9c10c0565815af2a5
elpablo <paolo.quadrani@gmail.com>
2010-10-07 18:24:29 +0200

    Basic implementation of mafInteractor.

---
+++ mafResources/mafInteractor.cpp
@@ -0,0 +1,35 @@
+/*
+ *  mafInteractor.m
+ *  mafResources
+ *
+ *  Created by Paolo Quadrani on 07/10/10.
+ *  Copyright 2009 B3C. All rights reserved.
+ *
+ *  See Licence at: http://tiny.cc/QXJ4D
+ *
+ */
+
+#include "mafInteractor.h"
+
+using namespace mafCore;
+using namespace mafResources;
+
+mafInteractor::mafInteractor(const mafString code_location) : mafObjectBase(code_location), m_DefaultInteractorStyle(NULL), m_EditingInteractorStyle(NULL) {
+}
+
+mafInteractor::mafInteractor(mafContainerInterfacePointer default_is, mafContainerInterfacePointer edit_is, const mafString code_location) : mafObjectBase(code_location), m_DefaultInteractorStyle(default_is), m_EditingInteractorStyle(edit_is) {
+}
+
+void mafInteractor::setDefaultInteractionStyle(mafContainerInterfacePointer is) {
+    if(is == m_DefaultInteractorStyle) {
+        return;
+    }
+    m_DefaultInteractorStyle = is;
+}
+
+void mafInteractor::setEditingInteractionStyle(mafContainerInterfacePointer is) {
+    if(is == m_EditingInteractorStyle) {
+        return;
+    }
+    m_EditingInteractorStyle = is;
+}
---
+++ mafResources/mafInteractor.h
@@ -0,0 +1,73 @@
+/*
+ *  mafInteractor.h
+ *  mafResources
+ *
+ *  Created by Paolo Quadrani on 07/10/10.
+ *  Copyright 2009 B3C. All rights reserved.
+ *
+ *  See Licence at: http://tiny.cc/QXJ4D
+ *
+ */
+
+#ifndef MAFINTERACTOR_H
+#define MAFINTERACTOR_H
+
+// Includes list
+#include "mafResourcesDefinitions.h"
+#include <mafContainerInterface.h>
+
+namespace mafResources {
+
+// Class forwarding list
+
+/**
+Class name: mafInteractor
+This class defines the base class for the interactor in MAF3.
+The interactor can have optionally a default style that will be considered during the VME selection
+from the input device and optionally an editing style which can be used during the editing operation of the data associated with the VME.
+@sa mafVME
+*/
+class MAFRESOURCESSHARED_EXPORT mafInteractor : public mafCore::mafObjectBase {
+    Q_OBJECT
+    /// typedef macro.
+    mafSuperclassMacro(mafCore::mafObjectBase);
+
+public:
+    /// Object constructor
+    mafInteractor(const mafString code_location = "");
+
+    /// Object constructor
+    mafInteractor(mafCore::mafContainerInterfacePointer default_is, mafCore::mafContainerInterfacePointer edit_is = NULL, const mafString code_location = "");
+
+    /// Allow to assign the default interaction style.
+    void setDefaultInteractionStyle(mafCore::mafContainerInterfacePointer is);
+
+    /// Return the default interaction style.
+    mafCore::mafContainerInterfacePointer defaultInteractionStyle();
+
+    /// Allow to assign the editing interaction style.
+    void setEditingInteractionStyle(mafCore::mafContainerInterfacePointer is);
+
+    /// Return the editing interaction style.
+    mafCore::mafContainerInterfacePointer editingInteractionStyle();
+
+private:
+    mafCore::mafContainerInterfacePointer m_DefaultInteractorStyle; ///< Default interaction style that will be associated with the mafVME.
+    mafCore::mafContainerInterfacePointer m_EditingInteractorStyle; ///< Interaction style associated to the mafVME and used during the editing operations of the data inside the mafVME itself.
+};
+
+/////////////////////////////////////////////////////////////
+// Inline methods
+/////////////////////////////////////////////////////////////
+
+inline mafCore::mafContainerInterfacePointer mafInteractor::defaultInteractionStyle() {
+    return m_DefaultInteractorStyle;
+}
+
+inline mafCore::mafContainerInterfacePointer mafInteractor::editingInteractionStyle() {
+    return m_EditingInteractorStyle;
+}
+
+} // namespace mafResources
+
+#endif // MAFINTERACTOR_H
