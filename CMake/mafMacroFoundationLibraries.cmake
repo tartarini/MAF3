42c4efd417ef931ac369e0b65b274b25a1cee0c0
Daniele Giunchi <d.giunchi@scsolutions.it>
2010-10-11 17:40:10 +0200

    add a common point to include foundation libraries

---
+++ CMake/mafMacroFoundationLibraries.cmake
@@ -0,0 +1,17 @@
+#
+#  mafMacroFoundationLibraries.cmake
+#  maf
+#
+#  Created by Daniele Giunchi - Paolo Quadrani on 10/09/10.
+#  Copyright 2009 B3C. All rights reserved.
+#
+#  See Licence at: http://tiny.cc/QXJ4D
+#
+#
+
+MACRO(mafMacroFoundationLibraries)
+include(FindVXL)
+include(FindQtSOAP)
+include(FindQXMLRPC)
+#include(FindQscxml)  
+ENDMACRO()
