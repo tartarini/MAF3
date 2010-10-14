1e2d814089d535b8b6330ad8e4042457ee463604
elpablo <paolo.quadrani@gmail.com>
2010-10-14 13:14:37 +0200

    Target library file used to assign dependences.

---
+++ mafPluginVTK/mafPluginVTKTest/target_libraries.cmake
@@ -0,0 +1,16 @@
+#
+# See CMake/mafMacroGetTargetLibraries.cmake
+# 
+# This file should list the libraries required to build the current MAF module.
+# 
+
+SET(target_libraries
+  ${MAF_BASE_LIBRARIES}
+  ${MAF_FOUNDATION_LIBRARIES}
+  mafCore
+  mafEventBus
+  mafResources
+  mafPluginVTK
+  mafQA
+  ${VTK_LIBRARY}
+  )
