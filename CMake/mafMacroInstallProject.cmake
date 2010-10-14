7da336725e5de3a9689c8b959c849c4df2c8706c
elpablo <paolo.quadrani@gmail.com>
2010-10-14 15:24:57 +0200

    Added license header.

--- CMake/mafMacroInstallProject.cmake
+++ CMake/mafMacroInstallProject.cmake
@@ -1,3 +1,14 @@
+#
+#  mafMacroInstallProject.cmake
+#  maf
+#
+#  Created by Daniele Giunchi - Paolo Quadrani on 10/09/10.
+#  Copyright 2009 B3C. All rights reserved.
+#
+#  See Licence at: http://tiny.cc/QXJ4D
+#
+#
+
 MACRO(mafMacroInstallProject)
 
   install(FILES ${${PROJECT_NAME}_HEADERS} DESTINATION include/${PROJECT_NAME})
