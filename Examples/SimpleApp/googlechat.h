f665b9a1e78f8c06e86dc5383418a4be11840132
elpablo <paolo.quadrani@gmail.com>
2010-10-27 18:03:16 +0200

    Code related to google chat.

---
+++ Examples/SimpleApp/googlechat.h
@@ -0,0 +1,31 @@
+#include <QWidget>
+#include <QWebElement>
+
+#include "ui_form.h"
+
+class GoogleChat: public QWidget
+{
+    Q_OBJECT
+
+public:
+    GoogleChat();
+
+protected:
+    void showStatus(const QString &msg);
+    void showError(const QString &msg);
+    QWebElement document() const;
+
+private slots:
+
+    void adjustLoginButton();
+    void inputPassword();
+    void doLogin();
+    void initialPage(bool ok);
+    void loginPage(bool ok);
+    void hideElements();
+
+private:
+    Ui::Form form;
+    QString userName;
+    QString password;
+};
