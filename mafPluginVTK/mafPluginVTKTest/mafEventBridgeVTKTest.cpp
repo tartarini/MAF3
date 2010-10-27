d1ce20e141f3032fa33cbaebcf84776e370a4c44
rmucci <r.mucci@cineca.it>
2010-10-27 11:14:03 +0200

    Add pick event, and create a sphere in picking point

--- mafPluginVTK/mafPluginVTKTest/mafEventBridgeVTKTest.cpp
+++ mafPluginVTK/mafPluginVTKTest/mafEventBridgeVTKTest.cpp
@@ -13,11 +13,17 @@
 #include <mafCoreSingletons.h>
 #include <mafResourcesRegistration.h>
 #include <mafEventBridgeVTK.h>
+#include <mafVisualPipeVTKSurface.h>
 
+#include <vtkSphereSource.h>
+#include <vtkSmartPointer.h>
 #include <vtkRenderWindowInteractor.h>
 #include <vtkRenderWindow.h>
 #include <vtkRenderer.h>
 #include <vtkCommand.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkActor.h>
+#include <vtkProperty.h>
 
 
 //using namespace mafEventBus;
@@ -37,30 +43,50 @@ public:
     /// Object constructor
     testInteractionManagerCustom(const mafString code_location = "");
 
+    double *position();
+
     int m_Counter; ///< Test variable;
+    double m_Pos[3]; ///< Test variable;
 
 
 public slots:
     /// observer needed to receive the 'maf.local.resources.interaction.leftButtonPress' signal
     void leftButtonPress();
 
-    void mouseMove();
+    /// observer needed to receive the 'maf.local.resources.interaction.vmePick' signal
+    void pick(double *pos, mafCore::mafContainerInterface *interface);
 
 signals:
     /// left button pressed.
     void leftButtonPressSignal();
 
+    /// picked button pressed.
+    void pickSignal(double *pos, mafCore::mafContainerInterface *interface);
+
 };
 
 testInteractionManagerCustom::testInteractionManagerCustom(QString code_location) : mafObjectBase(code_location) {
+    mafIdProvider::instance()->createNewId("maf.local.resources.interaction.vmePick");
     mafRegisterLocalSignal("maf.local.resources.interaction.leftButtonPress", this, "leftButtonPressSignal()");
     mafRegisterLocalCallback("maf.local.resources.interaction.leftButtonPress", this, "leftButtonPress()");
-    m_Counter = 0;
 
+    mafRegisterLocalSignal("maf.local.resources.interaction.vmePick", this, "pickSignal(double *,mafCore::mafContainerInterface *)");
+    mafRegisterLocalCallback("maf.local.resources.interaction.vmePick", this, "pick(double *, mafCore::mafContainerInterface *)");
+    m_Counter = 0;
 }
 
 void testInteractionManagerCustom::leftButtonPress() {
     m_Counter++;
+}
+
+void testInteractionManagerCustom::pick(double *pos, mafCore::mafContainerInterface *interface) {
+     m_Counter++;
+     m_Pos[0] = pos[0];
+     m_Pos[1]= pos[1];
+     m_Pos[2] = pos[2];
+}
+double *testInteractionManagerCustom::position() {
+    return m_Pos;
 }
 
 
@@ -83,6 +109,8 @@ private slots:
 
     /// Initialize test variables
     void initTestCase() {
+        mafResourcesRegistration::registerResourcesObjects();
+        mafRegisterObjectAndAcceptBind(mafPluginVTK::mafVisualPipeVTKSurface);
         m_CustomManager = mafNEW(testInteractionManagerCustom);
         m_EventBridge = mafNEW(mafPluginVTK::mafEventBridgeVTK);
     }
@@ -112,19 +140,56 @@ void mafEventBridgeVTKTest::mafEventBridgeVTKAllocationTest() {
 void mafEventBridgeVTKTest::mafEventBridgeVTKConnectionTest() {
     QVERIFY(m_EventBridge != NULL);
 
+    testInteractionManagerCustom *bridge = new testInteractionManagerCustom();
     vtkRenderWindow *renWin = vtkRenderWindow::New();
     vtkRenderer *renderer = vtkRenderer::New();
     vtkRenderWindowInteractor *iren = vtkRenderWindowInteractor::New();
 
+    //Create an Actor to be rendered
+    vtkSmartPointer<vtkSphereSource> sphereSource = vtkSmartPointer<vtkSphereSource>::New();
+    sphereSource->SetRadius(5);
+    sphereSource->Update();
+    vtkSmartPointer<vtkPolyDataMapper> sphereMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+    sphereMapper->SetInputConnection(sphereSource->GetOutputPort());
+    vtkSmartPointer<vtkActor> sphereActor = vtkSmartPointer<vtkActor>::New();
+    sphereActor->SetMapper(sphereMapper);
+
+    renderer->AddActor(sphereActor);
     renWin->AddRenderer(renderer);
     iren->SetRenderWindow(renWin);
 
     m_EventBridge->setInteractor(iren);
-    //Send some interaction events by VTK
+
+    renWin->Render();
+    QTest::qSleep(2000);
+
+    //Send some interaction events by VTK (left Button press picking the actor)
+    iren->SetEventPosition(201,138);
     iren->InvokeEvent(vtkCommand::LeftButtonPressEvent, NULL);
 
+    //Create a sphere on the picking position
+    vtkSmartPointer<vtkSphereSource> pickSphere = vtkSmartPointer<vtkSphereSource>::New();
+    pickSphere->Update();
+    vtkSmartPointer<vtkPolyDataMapper> pickSphereMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+    pickSphereMapper->SetInputConnection(pickSphere->GetOutputPort());
+    vtkSmartPointer<vtkActor> pickSphereActor = vtkSmartPointer<vtkActor>::New();
+    pickSphereActor->SetMapper(pickSphereMapper);
+    double *pos;
+    pos = bridge->position();
+
+    pickSphereActor->GetProperty()->SetColor(1,0,0);
+    pickSphere->SetCenter(bridge->position());
+    pickSphere->SetRadius(1);
+    pickSphere->Update();
+    renderer->AddActor(pickSphereActor);
+    renWin->Render();
+    QTest::qSleep(2000);
+
+
+
+
     //Check if events has been captured by testInteractionManagerCustom
-    QVERIFY(m_CustomManager->m_Counter == 1);
+    QVERIFY(m_CustomManager->m_Counter == 2);
 }
 
 MAF_REGISTER_TEST(mafEventBridgeVTKTest);
