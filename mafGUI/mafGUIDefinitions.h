4fe8df5c67d96b575e6c98996e7279566a8bc2a0
elpablo <paolo.quadrani@gmail.com>
2010-10-27 17:52:57 +0200

    File containing definition and other module's include files.

---
+++ mafGUI/mafGUIDefinitions.h
@@ -0,0 +1,22 @@
+/*
+ *  mafGUIDefinitions.h
+ *  mafGUI
+ *
+ *  Created by Paolo Quadrani on 27/10/10.
+ *  Copyright 2010 B3C. All rights reserved.
+ *
+ *  See Licence at: http://tiny.cc/QXJ4D
+ *
+ */
+
+#ifndef MAFGUIDEFINITIONS_H
+#define MAFGUIDEFINITIONS_H
+
+#include "mafGUI_global.h"
+
+#include <mafCoreDefinitions.h>
+#include <mafObjectFactory.h>
+#include <mafCoreSingletons.h>
+#include <mafCoreRegistration.h>
+
+#endif // MAFGUIDEFINITIONS_H
