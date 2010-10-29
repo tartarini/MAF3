d0f5b9c8b37c7598b86fdc777038ea9bcf0100d6
rmucci <r.mucci@cineca.it>
2010-10-29 15:46:46 +0200

    Added modifiers

--- mafResources/mafVisualPipe.h
+++ mafResources/mafVisualPipe.h
@@ -41,11 +41,11 @@ public:
     mafCore::mafContainerInterface *output();
 
 signals:
-    void vmePickSignal(double *pickPos, mafCore::mafContainerInterface *actor);
+    void vmePickSignal(double *pickPos, unsigned long,  mafCore::mafContainerInterface *actor);
 
 
 private slots:
-    void vmePick(double *pickPos, mafCore::mafContainerInterface *actor);
+    void vmePick(double *pickPos, unsigned long, mafCore::mafContainerInterface *actor);
 
 private:
     void initializeConnections();
