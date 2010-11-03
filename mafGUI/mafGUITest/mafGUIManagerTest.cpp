76d87424f1c059794cf7967f98a4c20b3378c102
elpablo <paolo.quadrani@gmail.com>
2010-11-03 10:07:36 +0100

    Added license header.

--- mafGUI/mafGUITest/mafGUIManagerTest.cpp
+++ mafGUI/mafGUITest/mafGUIManagerTest.cpp
@@ -1,3 +1,14 @@
+/*
+ *  mafGUIManagerTest.cpp
+ *  mafGUITest
+ *
+ *  Created by Paolo Quadrani on 26/10/10.
+ *  Copyright 2010 B3C. All rights reserved.
+ *
+ *  See Licence at: http://tiny.cc/QXJ4D
+ *
+ */
+
 #include <mafTestSuite.h>
 #include <mafCoreSingletons.h>
 #include <mafGUIRegistration.h>
