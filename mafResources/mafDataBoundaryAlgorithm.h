d11f93eddef68175026e46fd85211c153d6410de
rmucci <r.mucci@cineca.it>
2010-11-15 15:04:32 +0100

    Addewd calculateBoundary form bounds input

--- mafResources/mafDataBoundaryAlgorithm.h
+++ mafResources/mafDataBoundaryAlgorithm.h
@@ -32,7 +32,13 @@ public:
     mafDataBoundaryAlgorithm(const mafString code_location = "") : mafCore::mafObjectBase(code_location) {}
 
     /// Algorithm that will be used to extract the boundary from the given data value and pose matrix.
-    virtual mafCore::mafContainerInterface *calculateBoundary(mafCore::mafContainerInterface *data, mafPoseMatrix *matrix) = 0;
+    virtual mafCore::mafContainerInterface *calculateBoundary(mafCore::mafContainerInterface *data, mafPoseMatrix *matrix = NULL) = 0;
+
+    /// Algorithm that will be used to extract the boundary from the given bounds and pose matrix.
+    virtual mafCore::mafContainerInterface *calculateBoundary(double bounds[6], mafPoseMatrix *matrix = NULL) = 0;
+
+    /// Return bounds value of the bounding box.
+    virtual void bounds(double bounds[6]) = 0;
 };
 
 } // namespace mafResources
