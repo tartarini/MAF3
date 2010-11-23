f60be6ffde479c955e4cfca0b018837647edb0ae
rmucci <r.mucci@cineca.it>
2010-11-23 14:30:56 +0100

    Use vtkCubeSource

--- mafPluginVTK/mafPluginVTKTest/mafDataBoundaryAlgorithmVTKTest.cpp
+++ mafPluginVTK/mafPluginVTKTest/mafDataBoundaryAlgorithmVTKTest.cpp
@@ -51,42 +51,21 @@ private slots:
         mafResourcesRegistration::registerResourcesObjects();
 
         // Create a polydata.
-        vtkPolyData *surface = vtkPolyData::New();
-        vtkPoints *points = vtkPoints::New();
-        vtkCellArray *polys = vtkCellArray::New();
-        vtkFloatArray *scalars = vtkFloatArray::New();
+        m_DataSource = vtkCubeSource::New();
+        m_DataSource->SetXLength(5);
+        m_DataSource->SetYLength(3);
+        m_DataSource->SetZLength(8);
 
-        int i;
-        static float x[8][3]={{0,0,0}, {5,0,0}, {5,5,0}, {0,5,0},
-                        {0,0,5}, {5,0,1}, {6,5,2}, {0,6,5}};
-        static vtkIdType pts[6][4]={{0,1,2,3}, {4,5,6,7}, {0,1,5,4},
-                        {1,2,6,5}, {2,3,7,6}, {3,0,4,7}};
-        for (i=0; i<8; i++) points->InsertPoint(i,x[i]);
-        for (i=0; i<6; i++) polys->InsertNextCell(4,pts[i]);
-        for (i=0; i<8; i++) scalars->InsertTuple1(i,i);
-
-        // We now assign the pieces to the vtkPolyData.
-        surface->SetPoints(points);
-        surface->SetPolys(polys);
-        surface->GetPointData()->SetScalars(scalars);
-        points->Delete();
-        polys->Delete();
-        scalars->Delete();
-
-        m_Container.setDestructionFunction(&vtkPolyData::Delete);
-        m_Container.setExternalCodecType("mafPluginVTK::mafExternalDataCodecVTK");
-        m_Container.setExternalDataType("vtkPolydata");
-        m_Container = surface;
+        m_DataSourceContainer.setExternalCodecType("mafPluginVTK::mafExternalDataCodecVTK");
+        m_DataSourceContainer.setExternalDataType("vtkAlgorithmOutput");
+        m_DataSourceContainer = m_DataSource->GetOutputPort(0);
 
 
         mafPoseMatrix *newMatrix = new mafPoseMatrix();
         newMatrix->set_identity();
-        newMatrix->put(0,3,5);
-        newMatrix->put(1,3,2);
-        newMatrix->put(2,3,4);
 
         m_DataSetCube = mafNEW(mafResources::mafDataSet);
-        m_DataSetCube->setDataValue(&m_Container);
+        m_DataSetCube->setDataValue(&m_DataSourceContainer);
         m_DataSetCube->setPoseMatrix(newMatrix);
 
     }
@@ -94,6 +73,7 @@ private slots:
     /// Cleanup test variables memory allocation.
     void cleanupTestCase() {
         mafDEL(m_DataSetCube);
+        m_DataSource->Delete();
     }
 
     /// Test calculation of VTK boundary.
@@ -101,9 +81,9 @@ private slots:
 
 
 private:
+    vtkCubeSource *m_DataSource;
     mafDataSet *m_DataSetCube; ///< DataSet var.
-    mafContainer<vtkPolyData> m_PolyData; ///< Container of the vtkPolyData
-    mafContainer<vtkPolyData> m_Container; ///< Test var.
+    mafContainer<vtkAlgorithmOutput> m_DataSourceContainer; ///< Container of the Data Source
 };
 
 void mafDataBoundaryAlgorithmVTKTest::calculateBoundaryTest() {
@@ -119,7 +99,7 @@ void mafDataBoundaryAlgorithmVTKTest::calculateBoundaryTest() {
     double boundsOut[6];
     box->GetBounds(boundsOut);
 
-    double boundsIn[6] = {5,11,2,8,4,9};
+    double boundsIn[6] = {-2.5,2.5,-1.5,1.5,-4,4};
     QCOMPARE(boundsIn[0], boundsOut[0]);
     QCOMPARE(boundsIn[1], boundsOut[1]);
     QCOMPARE(boundsIn[2], boundsOut[2]);
