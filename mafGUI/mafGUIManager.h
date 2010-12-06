df25e6165ff03fd1e269b741f73ddf4e50bc6a91
elpablo <paolo.quadrani@gmail.com>
2010-12-06 14:33:42 +0100

    Introduced the managing of the recent files and the fill menu when a plugin is loaded.

--- mafGUI/mafGUIManager.h
+++ mafGUI/mafGUIManager.h
@@ -45,6 +45,7 @@ namespace mafGUI {
  */
 class MAFGUISHARED_EXPORT mafGUIManager : public mafCore::mafObjectBase {
     Q_OBJECT
+    Q_PROPERTY(int maxRecentFiles READ maxRecentFiles WRITE setMaxRecentFiles)
     /// typedef macro.
     mafSuperclassMacro(mafCore::mafObjectBase);
 
@@ -55,14 +56,17 @@ public:
     /// Object destructor.
     /* virtual */ ~mafGUIManager();
 
-    /// Create the actions associated with the menu items.
-    virtual void createActions();
-
     /// Create the menu for the vertical application.
     virtual void createMenus();
 
     /// Create the toolbar for the vertical application and add them to the main window.
     virtual void createToolBars();
+
+    /// Return the number of maximum recent files.
+    int maxRecentFiles();
+
+    /// Allows to assign the maximum number of recent files. Canges of this number will be considered at the application's sturtup.
+    void setMaxRecentFiles(int max_recent_files);
 
     /// Return the 'file' menu item created into the createMenus method.
     QMenu *fileMenu() const;
@@ -82,12 +86,16 @@ public:
     /// Show the file dialog to select the file path given a window title, a starting directory and an optional file wildcard
     void chooseFileDialog(const mafString title, const mafString start_dir, const mafString wildcard = mafTr("All files (*.*)"));
 
+protected:
+    /// Create the actions associated with the menu items.
+    virtual void createActions();
+
 public slots:
     /// Fill the operation and view menu with the plugged objects.
     void fillMenuWithPluggedObjects(mafCore::mafPluggedObjectsHash pluginHash);
 
 signals:
-    /// Signals emitted on path selection using the dialog.
+    /// Signal emitted on path selection using the dialog.
     void pathSelected(const mafString path);
 
 private slots:
@@ -97,11 +105,21 @@ private slots:
     /// Create the view corresponding to the view's action activated.
     void createView();
 
+    /// Allows to ask to open the selected recent file.
+    void openRecentFile();
+
 private:
     /// Initialize the topics and register the signals with the mafEventBus.
     void registerSignals();
 
+    /// Update the recent file action with new recent file items.
+    void updateRecentFileActions();
+
+    /// Manage the filename of the recent file.
+    mafString strippedName(const mafString &fullFileName);
+
     QMenu *m_FileMenu; ///< Reference to the 'File' menu.
+    QMenu *m_RecentFilesMenu; ///< Reference to 'Recent File' menu.
     QMenu *m_EditMenu; ///< Reference to the 'Edit' menu.
     QMenu *m_ViewMenu; ///< Reference to the 'View' menu.
     QMenu *m_OpMenu;   ///< Reference to the 'Operation' menu.
@@ -115,6 +133,7 @@ private:
     QAction *m_OpenAct; ///< Reference to the action associated to the 'Open' existing data.
     QAction *m_SaveAct; ///< Reference to the action associated to the 'Save' Items in a persistent way.
     QAction *m_SaveAsAct; ///< Reference to the action associated to the 'Save As' Items in a persistent way by assigning a name.
+    QAction *m_RecentFilesSeparatorAct; ///< Separator used to separate the recent files menu item from the previous one (if any recent is present)
     QAction *m_ExitAct; ///< Reference to the action associated to the 'Exit'.
 
     QAction *m_CutAct;  ///< Reference to the action associated to the 'Cut' of Items from actual loaded data.
@@ -122,6 +141,10 @@ private:
     QAction *m_PasteAct; ///< Reference to the action associated to the 'Paste' of Items into actual loaded data.
 
     QAction *m_AboutAct; ///< Reference to the action associated to the 'About' informative dialog.
+
+    int m_MaxRecentFiles; ///< Number of maximum recent files.
+    mafList<QAction *> m_RecentFileActs; ///< List of recent file's actions.
+    bool m_ActionsCreated; ///< Flag that store the actions' creation.
 
     QMainWindow *m_MainWindow; ///< Main window associated to the application.
 };
@@ -132,6 +155,14 @@ private:
 
 inline QMenu *mafGUIManager::fileMenu() const {
     return m_FileMenu;
+}
+
+inline void mafGUIManager::setMaxRecentFiles(int max_recent_files) {
+    m_MaxRecentFiles = max_recent_files;
+}
+
+inline int mafGUIManager::maxRecentFiles() {
+    return m_MaxRecentFiles;
 }
 
 inline QMenu *mafGUIManager::editMenu() const {
