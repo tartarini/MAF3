f333cd214cd4f8ef96107efb9d01cef4a608cb50
elpablo <paolo.quadrani@gmail.com>
2010-12-16 12:17:31 +0100

    Extended test suite to test the plugObject method.

--- mafApplicationLogic/mafApplicationLogicTest/mafLogicTest.cpp
+++ mafApplicationLogic/mafApplicationLogicTest/mafLogicTest.cpp
@@ -12,9 +12,30 @@
 #include "mafTestSuite.h"
 #include <mafLogic.h>
 #include <mafCoreSingletons.h>
+#include <mafObject.h>
 
 using namespace mafCore;
 using namespace mafApplicationLogic;
+using namespace mafEventBus;
+
+//----------------------------------------------------------------------
+
+/**
+ Class name: testObjectCustomPlugged
+ Test class used to test mafLogic::plugObject method.
+ */
+class testObjectCustomPlugged : public mafObject {
+    /// Typedef macro
+    mafSuperclassMacro(mafCore::mafObject);
+
+public:
+    testObjectCustomPlugged(const mafString code_location = "");
+
+};
+
+testObjectCustomPlugged::testObjectCustomPlugged(mafString code_location) : mafObject(code_location) {
+}
+//----------------------------------------------------------------------
 
 /**
  Class name: mafLogicTest
@@ -48,6 +69,9 @@ private slots:
     /// Test the working directory accessors.
     void workingDirectoryTest();
 
+    /// Test the plugObject functionality.
+    void plugObjectTest();
+
 private:
     mafLogic *m_Logic; ///< Test variable.
 };
@@ -75,6 +99,27 @@ void mafLogicTest::workingDirectoryTest() {
     m_Logic->setWorkingDirectory("/myBaseDir/myDir1");
     wd = m_Logic->workingDirectory();
     QVERIFY(res != wd);
+}
+
+void mafLogicTest::plugObjectTest() {
+    // Plug the custom object; it will be added to the plugin manager and available into the framework
+    m_Logic->plugObject("mafCore::mafObject", "testObjectCustomPlugged", "Custom Object");
+    m_Logic->loadPlugins();
+
+    // Query for the plugged object, so to verify the functionality.
+    mafEventArgumentsList argList;
+    mafString baseClassType("mafCore::mafObject");
+    argList.append(mafEventArgument(mafString, baseClassType));
+    mafPluggedObjectInformationList *pluggedObjectList = NULL;
+    mafGenericReturnArgument ret_val = mafEventReturnArgument(mafCore::mafPluggedObjectInformationList *, pluggedObjectList);
+    mafEventBusManager::instance()->notifyEvent("maf.local.resources.plugin.resourcesQuery", mafEventTypeLocal, &argList, &ret_val);
+
+    QVERIFY(pluggedObjectList != NULL);
+    int num = pluggedObjectList->size();
+    QVERIFY(num == 1);
+    mafPluggedObjectInformation info = pluggedObjectList->at(0);
+    mafString ct = info.m_ClassType;
+    QVERIFY(ct == "testObjectCustomPlugged");
 }
 
 MAF_REGISTER_TEST(mafLogicTest);
