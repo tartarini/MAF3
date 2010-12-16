cd11437e6a64e5b7a8514ea789060b273500ba3b
elpablo <paolo.quadrani@gmail.com>
2010-12-16 16:24:09 +0100

    Demo operation with associated a UI file.

---
+++ Examples/SimpleApp/mafOperationSimpleApp.cpp
@@ -0,0 +1,23 @@
+/*
+ *  mafOperationSimpleApp.cpp
+ *  SimpleApp
+ *
+ *  Created by Paolo Quadrani on 14/12/10.
+ *  Copyright 2010 B3C. All rights reserved.
+ *
+ *  See Licence at: http://tiny.cc/QXJ4D
+ *
+ */
+
+#include "mafOperationSimpleApp.h"
+
+using namespace mafResources;
+
+mafOperationSimpleApp::mafOperationSimpleApp(const mafString code_location) : mafOperation(code_location) {
+    m_UIFilename = "mafOperationSimpleApp.ui";
+}
+
+bool mafOperationSimpleApp::acceptObject(mafCore::mafObjectBase *obj) {
+    mafVME *vme = dynamic_cast<mafVME*>(obj);
+    return vme != NULL;
+}
