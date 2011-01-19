f6425c6b18df2e3f69d35954e4caf5f26ce0dcfd
elpablo <paolo.quadrani@gmail.com>
2011-01-19 14:16:47 +0100

    Added the mechanism to load the mafPluginVTK.

--- Examples/SimpleApp/main.cpp
+++ Examples/SimpleApp/main.cpp
@@ -5,13 +5,38 @@
 
 #include <mafLogic.h>
 
-int main(int argc, char *argv[])
-{
+#ifdef WIN32
+    #define VTK_PLUGIN_NAME "mafPluginVTK_d.mafplugin"
+#else
+    #define VTK_PLUGIN_NAME "libmafPluginVTK_debug.mafplugin"
+#endif
+
+int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     a.setOrganizationName("SCS");
     a.setOrganizationDomain("scsolutions.org");
     a.setApplicationName("SimpleApp");
     a.setApplicationVersion("1.0");
+
+    mafString pluginVTK;
+    pluginVTK.append(VTK_PLUGIN_NAME);
+
+    mafString p = QDir::currentPath();
+    p.append(QDir::separator());
+    p.append("plugins");
+
+    QDir pluginDir(p);
+    if(!pluginDir.exists(p)) {
+        pluginDir.mkdir(p);
+    }
+    p.append(QDir::separator());
+    p.append(VTK_PLUGIN_NAME);
+
+    // Remove a previous version of the plug-in just to be updated in a
+    // development phase.
+    bool res = QFile::remove(p);
+    // Copy the new one into the plugin directory.
+    res = QFile::copy(pluginVTK, p);
 
     mafApplicationLogic::mafLogic logic;
     logic.initialize();
@@ -24,6 +49,7 @@ int main(int argc, char *argv[])
     mafRegisterObjectAndAcceptBind(mafOperationSimpleApp);
     // Plug the object's information into the framework
     logic.plugObject("mafResources::mafOperation", "mafOperationSimpleApp", "Demo Operation");
+    logic.plugObject("mafResources::mafView", "mafPluginVTK::mafViewVTK", "View 3D");
 
     mafMainWindow w(&logic);
     logic.loadPlugins();
