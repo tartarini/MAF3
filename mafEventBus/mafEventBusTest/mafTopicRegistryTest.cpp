460addf6093fd9d1552ebf4ec2b7e0d3302e2282
rmucci <r.mucci@cineca.it>
2011-01-26 17:31:08 +0100

    test class for mafTopicRegistry

---
+++ mafEventBus/mafEventBusTest/mafTopicRegistryTest.cpp
@@ -0,0 +1,110 @@
+/*
+ *  mafTopicRegistryTest.cpp
+ *  mafEventBusTest
+ *
+ *  Created by Roberto Mucci on 26/01/11.
+ *  Copyright 2011 B3C. All rights reserved.
+ *
+ *  See Licence at: http://tiny.cc/QXJ4D
+ *
+ */
+
+#include <mafTestSuite.h>
+#include <mafTopicRegistry.h>
+
+using namespace mafEventBus;
+
+/**
+ Class name: mafTopicRegistryTest
+ This class implements the test suite for mafTopicRegistry.
+ */
+
+//! <title>
+//mafTopicRegistry
+//! </title>
+//! <description>
+//mafTopicRegistry provides the registration of topic and topic owner in a hash.
+//! </description>
+
+class mafTopicRegistryTest : public QObject {
+    Q_OBJECT
+
+private slots:
+    /// Initialize test variables
+    void initTestCase() {
+        m_TopicRegistry = mafTopicRegistry::instance();
+    }
+
+    /// Cleanup test variables memory allocation.
+    void cleanupTestCase() {
+        m_TopicRegistry->shutdown();
+    }
+
+    /// mafTopicRegistry registration test case.
+    void mafTopicRegistryRegisterTest();
+
+    /// mafTopicRegistry owner test case.
+    void mafTopicRegistryOwnerTest();
+
+
+private:
+    mafTopicRegistry *m_TopicRegistry; ///< Test var.
+
+};
+
+void mafTopicRegistryTest::mafTopicRegistryRegisterTest() {
+    QVERIFY(m_TopicRegistry != NULL);
+    mafString topic("maf.local.eventBus.testTopic");
+
+    bool result = m_TopicRegistry->registerTopic(topic, this);
+    QVERIFY(result);
+
+    result = m_TopicRegistry->registerTopic("maf.local.eventBus.testTopic1", this);
+    QVERIFY(result);
+
+    result = m_TopicRegistry->registerTopic("maf.local.eventBus.testTopic2", this);
+    QVERIFY(result);
+
+    result = m_TopicRegistry->registerTopic(topic, this);
+    QVERIFY(!result);
+
+    result = m_TopicRegistry->registerTopic("", this);
+    QVERIFY(!result);
+
+    result = m_TopicRegistry->registerTopic(topic, NULL);
+    QVERIFY(!result);
+
+    result = m_TopicRegistry->registerTopic("", NULL);
+    QVERIFY(!result);
+}
+
+void mafTopicRegistryTest::mafTopicRegistryOwnerTest() {
+    mafString topic("maf.local.eventBus.testTopic");
+    const QObject *obj = m_TopicRegistry->owner(topic);
+    QVERIFY(obj == this);
+
+    obj = m_TopicRegistry->owner("");
+    QVERIFY(obj == NULL);
+
+    obj = m_TopicRegistry->owner("maf.local.eventBus.TopicNotRegisterd");
+    QVERIFY(obj == NULL);
+
+    //Check isPresent() method.
+    bool result = m_TopicRegistry->isTopicRegistered(topic);
+    QVERIFY(result);
+
+    result = m_TopicRegistry->isTopicRegistered("");
+    QVERIFY(!result);
+
+    result = m_TopicRegistry->isTopicRegistered("maf.local.eventBus.TopicNotRegisterd");
+    QVERIFY(!result);
+
+    m_TopicRegistry->dump();
+}
+
+
+MAF_REGISTER_TEST(mafTopicRegistryTest);
+#include "mafTopicRegistryTest.moc"
+
+
+
