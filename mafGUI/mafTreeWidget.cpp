d421d235147b5c1601a829f0832972699f3fd760
elpablo <paolo.quadrani@gmail.com>
2011-02-22 14:50:03 +0100

    Added actions and created the contextual menu.

--- mafGUI/mafTreeWidget.cpp
+++ mafGUI/mafTreeWidget.cpp
@@ -10,8 +10,22 @@
  */
 
 #include "mafTreeWidget.h"
+#include <QMenu>
 
 using namespace mafGUI;
 
 mafTreeWidget::mafTreeWidget(QWidget *parent) : QTreeView(parent) {
+    m_ActionShow = new QAction(mafTr("Show"), this);
+    m_ActionDelete = new QAction(mafTr("Delete"), this);
+}
+
+void mafTreeWidget::contextMenuEvent(QContextMenuEvent *e) {
+    QList<QAction *> actions;
+    if (this->indexAt(e->pos()).isValid()) {
+        actions.append(m_ActionShow);
+        actions.append(m_ActionDelete);
+    }
+    if (actions.count() > 0) {
+        QMenu::exec(actions, e->globalPos());
+    }
 }
