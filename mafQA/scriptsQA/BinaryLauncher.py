0a33eda4745b8a67d706394f8819b3f2f117f98c
Daniele Giunchi <d.giunchi@scsolutions.it>
2011-02-28 17:00:22 +0100

    fix indentation error

--- mafQA/scriptsQA/BinaryLauncher.py
+++ mafQA/scriptsQA/BinaryLauncher.py
@@ -105,7 +105,7 @@ def main():
         sys.exit(2)
     
     for o, a in opts:
-         if o in ("-h", "--help"):
+        if o in ("-h", "--help"):
             usage()
             sys.exit()
         elif o in ("-d", "--directory"):
