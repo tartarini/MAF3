2e41db1d9fbe94684c36aa0f508e0010616ccdf2
Daniele Giunchi <d.giunchi@scsolutions.it>
2011-03-01 12:55:18 +0100

    removed non-sense definitions

--- mafSerialization/mafCSVReader.h
+++ mafSerialization/mafCSVReader.h
@@ -38,25 +38,25 @@ class MAFSERIALIZATIONSHARED_EXPORT mafCSVReader : public mafCore::mafObjectBase
 
 public:
         /// Object constructor.
-        mafCSVReader(const mafString code_location = "");
+        mafCSVReader(const QString code_location = "");
         /// Overloaded object constructor
-        mafCSVReader(mafIODevice *device, const mafString code_location = "");
+        mafCSVReader(mafIODevice *device, const QString code_location = "");
         /// Overloaded object constructor
-        mafCSVReader(mafString &string, const mafString code_location = "");
+        mafCSVReader(mafString &string, const QString code_location = "");
         /// Object Destructor
         ~mafCSVReader();
 
         /// set the current string that will be parsed, string variable. It calls also initialize method before set the string. Reset device source.
-        void setString(mafString &string);
+        void setString(QString &string);
         /// set the current device from which read csv. It calls also initialize method before set the string. Reset string source.
-        void setDevice(mafIODevice *device);
+        void setDevice(QIODevice *device);
 
         /// it returns a line of the file or the string (each line need to terminate with \n.
         mafString readLine();
         /// parse a line and retrieve an ordered list of string.
         mafStringList parseLine();
         /// static method allow to be used like a global function.
-        static mafStringList parseLine(mafString line);
+        static QStringList parseLine(QString line);
 
         /// set the QTextCodec giving a specific name for it.
         void setTextCodec(const char* codecName);
@@ -64,9 +64,9 @@ private:
         /// reset position,regular expression and use the default QtextCode.
         void initialize();
 
-        mafIODevice *m_Device; ///< device which represents the source of csv input.
+        QIODevice *m_Device; ///< device which represents the source of csv input.
         QTextCodec *m_Codec; ///< provides conversions between text encodings.
-        mafString m_String; ///< string which represents the source of csv input.
+        QString m_String; ///< string which represents the source of csv input.
         int m_Position; ///< internal position of the input source.
         QRegExp m_RegularExpression; ///< regular expression for determine which is the character separator.
 	
