5b6fe928b5c6de62065b936246cfa276882eaabd
Daniele Giunchi <d.giunchi@scsolutions.it>
2011-03-04 11:02:54 +0100

    remove deprecated warning of QVTKWidget

--- CMake/mafMacroInitProject.cmake
+++ CMake/mafMacroInitProject.cmake
@@ -36,6 +36,8 @@ MACRO(mafMacroInitProject test)
   ## Add the project binary dir as include dir for the .moc files.
   INCLUDE_DIRECTORIES("${PROJECT_BINARY_DIR}")
 
+  ADD_DEFINITIONS(-Wno-deprecated)
+
   set(MY_MOC_CXX)
   set(MY_UI_CXX)
   if(${test})
