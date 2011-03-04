f558343358356c698a6d83e14cbacd1b127c7a43
rmucci <r.mucci@cineca.it>
2011-03-04 17:45:10 +0100

    manage lock status of  VME

--- mafGUI/mafTreeItemDelegate.cpp
+++ mafGUI/mafTreeItemDelegate.cpp
@@ -19,6 +19,9 @@
 #include <QStandardItem>
 #include <QPixmap>
 #include <QBitmap>
+#include <QCheckBox>
+#include <QLineEdit>
+#include <QPixmapCache>
 
 
 using namespace mafCore;
@@ -27,55 +30,109 @@ using namespace mafGUI;
 mafTreeItemDelegate::mafTreeItemDelegate(QObject *parent) : QStyledItemDelegate(parent) {
 }
 
-void mafTreeItemDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const {
+//QWidget *mafTreeItemDelegate::createEditor( QWidget * parent, const QStyleOptionViewItem & option, const QModelIndex & index ) const {
+//    QLineEdit *textEditor = new QLineEdit(parent);
+//    mafTreeItem *item = (mafTreeItem *)((QStandardItemModel *)index.model())->itemFromIndex(index);
+//    QObject *objItem = item->data();
+//    //textEditor->setText(objItem->property("objectName").toString());
+//    return textEditor;
 
-    //Control checkbox status
-    bool checked = index.model()->data(index, Qt::CheckStateRole).toBool();
-    QStyleOptionButton check_box_style_option;
-    check_box_style_option.state |= QStyle::State_Enabled;
-    if (checked) {
-        check_box_style_option.state |= QStyle::State_On;
-    } else {
-        check_box_style_option.state |= QStyle::State_Off;
+//}
+
+void mafTreeItemDelegate::setEditorData( QWidget * editor, const QModelIndex & index ) const {
+    QString text = index.model()->data(index, Qt::EditRole).toString();
+    QLineEdit *textEditor = static_cast<QLineEdit*>(editor);
+    textEditor->setText(text);
+    //warning: not working for root!!
+}
+
+void mafTreeItemDelegate::setModelData(QWidget * editor, QAbstractItemModel * model, const QModelIndex & index ) const {
+    //Get new VME name.
+    QLineEdit *textEditor = static_cast<QLineEdit*>(editor);
+    QString value = textEditor->text();
+    model->setData(index, value, Qt::EditRole);
+
+    //Set new VME name to the VME property.
+    mafTreeItem *item = (mafTreeItem *)((QStandardItemModel *)index.model())->itemFromIndex(index);
+    QObject *objItem = item->data();
+    objItem->setProperty("objectName", value );
+}
+
+void mafTreeItemDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const {
+    //Check if model is a mafSceneNode and set property visibility.
+    mafTreeItem *item = (mafTreeItem *)((QStandardItemModel *)index.model())->itemFromIndex(index);
+    QObject *objItem = item->data();
+
+    QPalette::ColorRole textColorRole = QPalette::NoRole;
+    QString iconFile;
+    QPixmap icon;
+    //Get lock status
+    int lockStatus = objItem->property("lockStatus").toInt();
+    switch (lockStatus) {
+        case mafCore::mafObjectLockNone: {
+            //Draw CheckBox
+            QStyleOptionButton checkBoxOption;
+            checkBoxOption.rect = option.rect.adjusted(1, 1, -1, -1);
+            checkBoxOption.state |= QStyle::State_Enabled; //diable in some cases.
+
+            //Control checkbox status
+            bool checkStatus = index.model()->data(index, Qt::CheckStateRole).toBool();
+            if (checkStatus) {
+                checkBoxOption.state |= QStyle::State_On;
+            } else {
+                checkBoxOption.state |= QStyle::State_Off;
+            }
+
+            QString objType = objItem->metaObject()->className();
+            if(objType.compare("mafResources::mafSceneNode") == 0){
+                bool visibility = objItem->property("visibility").toBool();
+                if(visibility != checkStatus) {
+                    objItem->setProperty("visibility", checkStatus);
+                }
+            }
+            QApplication::style()->drawControl(QStyle::CE_CheckBox, &checkBoxOption, painter, 0);
+          break;
+      }
+        case mafCore::mafObjectLockRead: {
+            //and case mafCore::mafObjectLockWrite:
+            //Draw lock Icon.
+            icon = QPixmap(":/images/lock_icon.png");
+            textColorRole = QPalette::Mid;
+            //Draw Icon.
+            QApplication::style()->drawItemPixmap(painter, option.rect, Qt::AlignLeft | Qt::AlignVCenter, icon);
+        break;
+        }
     }
 
-    check_box_style_option.rect = option.rect.adjusted(1, 1, -1, -1);
-    QApplication::style()->drawControl(QStyle::CE_CheckBox, &check_box_style_option, painter, 0);
-
     //Set icon beside checkbox
-    int x = option.rect.x() + 20; //replace 20 with the width of the checkbox.
+    int x = option.rect.x() +20; //replace 20 with the width of the checkbox.
     int y = option.rect.y();
     int w = option.rect.width() ;
     int h = option.rect.height();
 
-    //Get the VME inside the item
-    mafTreeItem *item = (mafTreeItem *)((QStandardItemModel *)index.model())->itemFromIndex(index);
-    QObject *objItem = item->data();
-
-    //Get path to the icon
-    QString iconFile;
-    QPixmap icon;
-    if(!objItem->property("canRead").toBool()) {
-        //locked image
-        icon = QPixmap(":/images/CLOSE_SASH.bmp");
-    } else {
-        iconFile = objItem->property("iconFile").toString();
-        icon = QPixmap(iconFile);
-    }
+    iconFile = objItem->property("iconFile").toString();
+    icon = QPixmap(iconFile,);
     if(icon.isNull()) {
-       //default image
-       icon = QPixmap(":/images/VME_SURFACE.bmp");
+        //default image
+        //icon = QPixmap(":/images/questionMark.png");
+        icon = QPixmap(":/images/lock_icon.png");
+        textColorRole = QPalette::Mid;
     }
+
+    //Draw Icon.
     QApplication::style()->drawItemPixmap(painter, QRect(x, y, w, h), Qt::AlignLeft | Qt::AlignVCenter, icon);
 
-    //Get the name of the VME
-    QString itemName = objItem->property("objectName").toString();
-
     //Set VME name beside icon
-    int newX = x + icon.rect().width() + 5; //set 2 to create a little space..
-    QApplication::style()->drawItemText(painter, QRect(newX, y, w, h), Qt::AlignLeft | Qt::AlignVCenter, QApplication::palette(), option.state, itemName);
+    int newX = x + icon.rect().width() + 5; //set 5 to create a little space..
+
+    //Get the name of the Object
+    QString itemName = objItem->property("objectName").toString();
+    //Draw VME name.
+    QApplication::style()->drawItemText(painter, QRect(newX, y, w, h), Qt::AlignLeft | Qt::AlignVCenter, QApplication::palette(), option.state, itemName, textColorRole);
 }
 
-QSize mafTreeItemDelegate::sizeHint (const QStyleOptionViewItem &option, const QModelIndex &index) const {
+QSize mafTreeItemDelegate::sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const {
         return QSize(20,20);
-    }
+}
+
+
--- mafGUI/mafTreeItemDelegate.h
+++ mafGUI/mafTreeItemDelegate.h
@@ -32,14 +32,29 @@ public:
     /// Object constructor.
     /*explicit*/ mafTreeItemDelegate(QObject *parent = 0);
 
+    /// Returns the editor to be used for editing the data item with the given index.
+    /*virtual */ //QWidget *createEditor ( QWidget * parent, const QStyleOptionViewItem & option, const QModelIndex & index ) const;
+
+    /// Sets the contents of the given editor to the data for the item at the given index.
+    /*virtual */ void setEditorData( QWidget * editor, const QModelIndex & index ) const;
+
+    /// Gets data from the editor widget and stores it in the specified model at the item index.
+    /*virtual */ void setModelData ( QWidget * editor, QAbstractItemModel * model, const QModelIndex & index ) const;
+
     /// Renders the delegate using the given painter and style option for the item specified by index.
     /*virtual */ void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const;
 
     /// Returns the size needed by the delegate to display the item specified by index.
     /*virtual */ QSize sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const;
 
+signals:
+    /// Alert observers about checkbox status.
+    void checkBoxStatus(bool status, const QModelIndex &index) const;
+
+public slots:
 
 private:
+
 
 
 };
