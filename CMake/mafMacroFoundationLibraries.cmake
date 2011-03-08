552f1dd075f5e14eaf884c0ebdc944854131cfc7
Daniele Giunchi <d.giunchi@scsolutions.it>
2011-03-08 20:03:10 +0100

    add macro for copy shared lib in install dir

--- CMake/mafMacroFoundationLibraries.cmake
+++ CMake/mafMacroFoundationLibraries.cmake
@@ -8,6 +8,27 @@
 #  See Licence at: http://tiny.cc/QXJ4D
 #
 #
+MACRO(mafCopyFoundationLibraries vardir)
+#copy all the libraries inside Install/bin
+
+SET(result)
+SET(erro)
+FILE(GLOB file_list ${vardir}/*.*)
+
+foreach(file ${file_list})
+get_filename_component(fileName ${file} NAME)
+
+set(result)
+set(erro)
+
+EXECUTE_PROCESS(
+   COMMAND ${CMAKE_COMMAND} -E copy
+   ${file}
+   ${CMAKE_BINARY_DIR}/Install/bin/${CMAKE_CFG_INTDIR}/${fileName}
+   RESULT_VARIABLE result
+   ERROR_VARIABLE erro)
+endforeach(file ${filelist})
+ENDMACRO()
 
 MACRO(mafMacroFoundationLibraries)
 
@@ -17,5 +38,8 @@ include(FindQXMLRPC)
 include(FindQScxml)
 
 set(MAF_FOUNDATION_LIBRARIES ${QXMLRPC_LIBRARY} ${VXL_LIBRARY} ${QTSOAP_LIBRARY} ${QSCXML_LIBRARY})
-
+mafCopyFoundationLibraries("${QXMLRPC_DIR}lib")
+mafCopyFoundationLibraries("${QtSOAP_DIR}lib")
+mafCopyFoundationLibraries("${QSCXML_DIR}lib")
 ENDMACRO()
+
