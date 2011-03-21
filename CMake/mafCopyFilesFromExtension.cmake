e59e0820d1478fc79c35d9341afc2e56b81c1744
elpablo <paolo.quadrani@gmail.com>
2011-03-21 18:57:43 +0100

    Changed EXECUTE_PROCESS into add_custom_command to copy correctly files with gived extensions at the right time.

--- CMake/mafCopyFilesFromExtension.cmake
+++ CMake/mafCopyFilesFromExtension.cmake
@@ -1,5 +1,5 @@
 #
-#  mafCopySharedLibrariesInInstallDir.cmake
+#  mafCopyFilesFromExtension.cmake
 #  maf
 #
 #  Created by Daniele Giunchi - Paolo Quadrani on 10/09/10.
@@ -8,21 +8,19 @@
 #  See Licence at: http://tiny.cc/QXJ4D
 #
 #
-MACRO(mafCopyFilesFromExtension extension)
+MACRO(mafCopyFilesFromExtension extension project)
+
+set(TARGET_LOC)
+set(DEST_ABSOLUTE_DIR)
+get_target_property(TARGET_LOC ${project} LOCATION)
+get_filename_component(DEST_ABSOLUTE_DIR ${TARGET_LOC} PATH)
 
 FILE(GLOB tocopy_file_list "${PROJECT_SOURCE_DIR}/*.${extension}")
 
 foreach(file ${tocopy_file_list})
   get_filename_component(fileName ${file} NAME)
-  set(result)
-  set(erro)
-  EXECUTE_PROCESS(
-     COMMAND ${CMAKE_COMMAND} -E copy
-     ${file}
-     ${CMAKE_BINARY_DIR}/Install/bin/${CMAKE_BUILD_TYPE}/${fileName}
-     RESULT_VARIABLE result
-     ERROR_VARIABLE erro)
-     
+  add_custom_command(TARGET ${project} POST_BUILD
+        COMMAND ${CMAKE_COMMAND} -E copy ${file} ${DEST_ABSOLUTE_DIR}/${fileName} )
 endforeach(file ${tocopy_file_list})
 
 ENDMACRO()
