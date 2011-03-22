b3be0007ca2eceb43f23756363a789713602abb3
Daniele Giunchi <d.giunchi@scsolutions.it>
2011-03-22 13:44:41 +0100

    add awful code for copy foundation lib files in install dir

--- CMake/mafMacroInstallProject.cmake
+++ CMake/mafMacroInstallProject.cmake
@@ -10,6 +10,32 @@
 #
 
 MACRO(mafMacroInstallProject)
+  # AWFUL CODE!!!!!! 
+  # to be removed as soon as git repositories for foundation libraries comes !!!!!
+  # 
+  FOREACH(foundation_library ${foundation_libraries})
+    if(${foundation_library} MATCHES "qxmlrpc")
+      mafCopySharedLibrariesInInstallDir("${QXMLRPC_DIR}lib")
+    endif(${foundation_library} MATCHES "qxmlrpc")
+    
+    if(${foundation_library} MATCHES "QtSolutions_SOAP-2.7")
+      mafCopySharedLibrariesInInstallDir("${QtSOAP_DIR}lib")
+    endif(${foundation_library} MATCHES "QtSolutions_SOAP-2.7")
+    
+    if(${foundation_library} MATCHES "qscxml")
+      mafCopySharedLibrariesInInstallDir("${QSCXML_DIR}lib")
+    endif(${foundation_library} MATCHES "qscxml")
+
+  ENDFOREACH()
+  
+  FOREACH(target_library ${target_libraries})
+    if(${target_library} MATCHES "vtkCommon")
+      mafCopySharedLibrariesInInstallDir("${VTK_DIR}lib")
+    endif(${target_library} MATCHES "vtkCommon")
+  ENDFOREACH()
+  
+  # END AWFUL CODE
+
   SET_TARGET_PROPERTIES( ${PROJECT_NAME} PROPERTIES PREFIX "" )
   if (CMAKE_BUILD_TYPE MATCHES Release)
       SET_TARGET_PROPERTIES( ${PROJECT_NAME} PROPERTIES OUTPUT_NAME "${PROJECT_NAME}" )
