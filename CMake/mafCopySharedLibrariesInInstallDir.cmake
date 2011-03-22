06ad883d314eafbc167e5fb5454f966f8fd26267
Daniele Giunchi <d.giunchi@scsolutions.it>
2011-03-22 13:43:28 +0100

    Project name instead of macro variable

--- CMake/mafCopySharedLibrariesInInstallDir.cmake
+++ CMake/mafCopySharedLibrariesInInstallDir.cmake
@@ -15,7 +15,7 @@ SET(result)
 SET(erro)
 FILE(GLOB file_list ${vardir}/*.*)
 
-get_target_property(TARGET_LOC mafCore LOCATION)
+get_target_property(TARGET_LOC ${PROJECT_NAME} LOCATION)
 get_filename_component(TARGET_ABSOLUTE_DIR ${TARGET_LOC} PATH)
 
 foreach(file ${file_list})
@@ -24,7 +24,7 @@ get_filename_component(fileName ${file} NAME)
 set(result)
 set(erro)
 
-add_custom_command(TARGET mafCore POST_BUILD
+add_custom_command(TARGET ${PROJECT_NAME} POST_BUILD
         COMMAND ${CMAKE_COMMAND} -E copy ${file} ${TARGET_ABSOLUTE_DIR}/${fileName} )
 
 endforeach(file ${filelist})
