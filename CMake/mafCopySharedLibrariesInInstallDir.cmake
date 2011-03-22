084f5bd8fc1e10874ed07f1d681b060f36215b5a
rmucci <r.mucci@cineca.it>
2011-03-22 16:31:04 +0100

    removed ET(result) and SET(erro)

--- CMake/mafCopySharedLibrariesInInstallDir.cmake
+++ CMake/mafCopySharedLibrariesInInstallDir.cmake
@@ -10,22 +10,12 @@
 #
 MACRO(mafCopySharedLibrariesInInstallDir vardir)
 #copy all the libraries inside Install/bin
-
-SET(result)
-SET(erro)
 FILE(GLOB file_list ${vardir}/*.*)
-
 get_target_property(TARGET_LOC ${PROJECT_NAME} LOCATION)
 get_filename_component(TARGET_ABSOLUTE_DIR ${TARGET_LOC} PATH)
-
 foreach(file ${file_list})
 get_filename_component(fileName ${file} NAME)
-
-set(result)
-set(erro)
-
 add_custom_command(TARGET ${PROJECT_NAME} POST_BUILD
         COMMAND ${CMAKE_COMMAND} -E copy ${file} ${TARGET_ABSOLUTE_DIR}/${fileName} )
-
 endforeach(file ${filelist})
 ENDMACRO()
