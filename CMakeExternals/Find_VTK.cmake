6d3404fd09013e602ce7e2e2dcf1e2238a4dfe9c
Daniele Giunchi <d.giunchi@scsitaly.com>
2011-04-04 16:06:39 +0200

    adjust find for vtk, add files from last update from master branch

---
+++ CMakeExternals/Find_VTK.cmake
@@ -0,0 +1,6 @@
+# need ${VTK_DIR} that is be set by ExternalProject_Add for vtk
+SET(VTK_DIR "${MAF_EXTERNAL_BUILD_DIR}/Build/VTK")
+FIND_PACKAGE(VTK REQUIRED)
+#this set is because, inside VTK 5, nobody fills it
+SET(VTK_CMAKE_DIR "${MAF_EXTERNAL_BUILD_DIR}/Source/VTK/Cmake")
+INCLUDE(${VTK_USE_FILE})
