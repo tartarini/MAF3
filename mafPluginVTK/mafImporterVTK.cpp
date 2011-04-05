606494c78feafeb63db4d855a509d43311664b78
elpablo <paolo.quadrani@gmail.com>
2011-04-05 13:30:47 +0200

    Upgraded to inherit from the mafImporter base class.

--- mafPluginVTK/mafImporterVTK.cpp
+++ mafPluginVTK/mafImporterVTK.cpp
@@ -10,44 +10,20 @@
  */
 
 #include "mafImporterVTK.h"
-#include <mafContainer.h>
-#include <vtkAlgorithmOutput.h>
 
 using namespace mafPluginVTK;
 using namespace mafResources;
-using namespace mafEventBus;
-using namespace mafCore;
 
-mafImporterVTK::mafImporterVTK(const QString code_location) : mafOperation(code_location), m_Filename(""), m_DataSet(NULL), m_VME(NULL), m_DataImported(false) {
-    m_OperationType = mafOperationTypeImporter;
-    m_UIFilename = "mafImporterVTK.ui";
+mafImporterVTK::mafImporterVTK(const QString code_location) : mafImporter(code_location) {
 }
 
 mafImporterVTK::~mafImporterVTK() {
-    cleanup();
-}
-
-bool mafImporterVTK::acceptObject(mafCore::mafObjectBase *obj) {
-    return true;
-}
-
-void mafImporterVTK::cleanup() {
-    // Cleanup memory and deregister callback.
-    mafDEL(m_DataSet);
-    mafDEL(m_Output);
-    mafUnregisterLocalCallback("maf.local.serialization.extDataImported", this, "importedVTKData(mafCore::mafContainerInterface *)")
 }
 
 void mafImporterVTK::execute() {
-    if (m_Filename.isEmpty()) {
-        qWarning() << mafTr("Filename of data to import is needed.");
-    } else {
-        mafRegisterLocalCallback("maf.local.serialization.extDataImported", this , "importedVTKData(mafCore::mafContainerInterface *)")
-        QString encodeType = "VTK";
-        mafEventArgumentsList argList;
-        argList.append(mafEventArgument(QString, m_Filename));
-        argList.append(mafEventArgument(QString, encodeType));
-        mafEventBusManager::instance()->notifyEvent("maf.local.serialization.import", mafEventTypeLocal, &argList);
+    setEncodeType("VTK");
+    if (!filename().isEmpty()) {
+        requestToImportFile();
         while (!m_DataImported || !m_Abort) {
             ;
         }
@@ -60,35 +36,3 @@ void mafImporterVTK::execute() {
     emit executionEnded();
 }
 
-void mafImporterVTK::importedVTKData(mafCore::mafContainerInterface *data) {
-    mafUnregisterLocalCallback("maf.local.serialization.extDataImported", this, "importedVTKData(mafCore::mafContainerInterface *)")
-
-    QFileInfo info(m_Filename);
-    
-    //Insert data into VME
-    m_VME = mafNEW(mafResources::mafVME);
-    m_VME->setObjectName(info.baseName());
-    m_DataSet = mafNEW(mafResources::mafDataSet);
-    m_DataSet->setDataValue(data);
-    m_VME->dataSetCollection()->insertItem(m_DataSet, 0);
-    this->m_Output = m_VME;
-
-    //Notify vme add
-    mafEventArgumentsList argList;
-    argList.append(mafEventArgument(mafCore::mafObjectBase *, m_Output));
-    mafEventBusManager::instance()->notifyEvent("maf.local.resources.vme.add", mafEventTypeLocal, &argList);
-    
-    m_DataImported = true;
-}
-
-void mafImporterVTK::unDo() {
-    mafEventArgumentsList argList;
-    argList.append(mafEventArgument(mafCore::mafObjectBase *, m_Output));
-    mafEventBusManager::instance()->notifyEvent("maf.local.resources.vme.remove", mafEventTypeLocal, &argList);
-}
-
-void mafImporterVTK::reDo() {
-    mafEventArgumentsList argList;
-    argList.append(mafEventArgument(mafCore::mafObjectBase *, m_Output));
-    mafEventBusManager::instance()->notifyEvent("maf.local.resources.vme.add", mafEventTypeLocal, &argList);
-}
