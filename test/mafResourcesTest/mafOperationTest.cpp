00984c7241f332cc60b05534119ee2a01965d270
rmucci <r.mucci@cineca.it>
2011-04-13 17:42:47 +0200

    added true

--- test/mafResourcesTest/mafOperationTest.cpp
+++ test/mafResourcesTest/mafOperationTest.cpp
@@ -62,7 +62,7 @@ void mafOperationTest::mafOperationAllocationTest() {
     QVERIFY ( m_Operation->isRunning() );
     m_Operation->unDo();
     m_Operation->reDo();
-    m_Operation->terminate();
+    m_Operation->terminate(true);
     QVERIFY ( !m_Operation->isRunning() );
 }
 
