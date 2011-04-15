ed01ce698ef158a44b279bd7f3f771e863733dc4
elpablo <paolo.quadrani@gmail.com>
2011-04-15 21:19:44 +0200

    Reorganized code to avoid to send import request to serialization module and removed related variables.

--- src/mafResources/mafImporter.h
+++ src/mafResources/mafImporter.h
@@ -44,6 +44,24 @@ public:
     /// Return the filename of the VTK data to import.
     QString filename() const;
     
+protected:
+    /// Object destructor.
+    /* virtual */ ~mafImporter();
+    
+    /// Request, through the mafEventBus, to import the data corresponding to the m_Filename.
+    void checkImportFile();
+    
+    /// Method called when the operation has been aborted from the user.
+    void cleanup();
+    
+    /// Obtain the imported data, put it into a mafVME add it to the data hierarchy.
+    virtual void importedData(mafCore::mafContainerInterface *data);
+    
+    mafVME *m_VME;  ///< VME containing the imported data coming from the mafSerialization module.
+
+private:
+    QString m_Filename; ///< Filename of external data to import.
+
 public slots:
     /// Allows to call the piece of algorithm that is needed to restore the previous state of the operation's execution.
     /*virtual*/ void unDo();
@@ -53,31 +71,6 @@ public slots:
     
     /// Set parameters of operation.
     /*virtual*/ void setParameters(QVariantList parameters);
-
-protected:
-    /// Object destructor.
-    /* virtual */ ~mafImporter();
-    
-    /// Request, through the mafEventBus, to import the data corresponding to the m_Filename.
-    void requestToImportFile();
-    
-    /// Allow to assign the encode type for the external data to be imported.
-    void setEncodeType(const QString encode_type);
-
-    /// Method called when the operation has been aborted from the user.
-    void cleanup();
-    
-    mafVME *m_VME;  ///< VME containing the imported data coming from the mafSerialization module.
-
-    volatile bool m_DataImported; ///< Flag used to monitor the imported data.
-
-private slots:
-    /// Obtain the imported data, put it into a mafVME add it to the data hierarchy.
-    virtual void importedData(mafCore::mafContainerInterface *data);
-    
-private:
-    QString m_EncodeType; ///< Encode type of the external data to be imported (e.g. VTK, STL, ...)
-    QString m_Filename; ///< Filename of external data to import.
 };
     
 inline void mafImporter::setFilename(const QString f) {
@@ -86,10 +79,6 @@ inline void mafImporter::setFilename(const QString f) {
     
 inline QString mafImporter::filename() const {
     return m_Filename;
-}
-    
-inline void mafImporter::setEncodeType(const QString encode_type) {
-    m_EncodeType = encode_type;
 }
 
 } // namespace mafPluginVTK
