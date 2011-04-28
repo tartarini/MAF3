83d1ac9136de1067ed716428c99dab7140a8abaa
elpablo <paolo.quadrani@gmail.com>
2011-04-28 13:32:06 +0200

    Call initialize for the operation when it starts.

--- src/mafResources/mafOperationManager.cpp
+++ src/mafResources/mafOperationManager.cpp
@@ -169,6 +169,7 @@ void mafOperationManager::startOperation(const QString operation) {
         return;
     }
 
+    m_CurrentOperation->initialize();
     // Assign as input the current selected VME.
     m_CurrentOperation->setInput(m_SelectedVME);
 
