cea2e0e08d51a1c8e71938b383ebf45796bcf411
Paolo Quadrani <paolo.quadrani@gmail.com>
2011-05-05 14:11:49 +0200

    Removed the copy shared library macro call.

--- CMakeExternals/Find_VTK.cmake
+++ CMakeExternals/Find_VTK.cmake
@@ -1,9 +1,7 @@
-# need ${VTK_DIR} that is be set by ExternalProject_Add for vtk
+# need ${VTK_DIR} that is set by ExternalProject_Add for vtk
 SET(VTK_DIR "${MAF_EXTERNAL_BUILD_DIR}/Build/VTK")
 FIND_PACKAGE(VTK REQUIRED)
 #this set is because, inside VTK 5, nobody fills it
 SET(VTK_CMAKE_DIR "${MAF_EXTERNAL_BUILD_DIR}/Source/VTK/CMake")
 INCLUDE(${VTK_USE_FILE})
 
-#copy files
-mafCopySharedLibrariesInInstallDir("${VTK_DIR}lib")
