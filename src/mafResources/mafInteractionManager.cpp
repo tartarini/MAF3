8bbd2c819a62ff58fd97ab9ec64af6555752eaa6
Paolo Quadrani <paolo.quadrani@gmail.com>
2011-05-06 13:04:11 +0200

    Added argument to the mouse management methods.

--- src/mafResources/mafInteractionManager.cpp
+++ src/mafResources/mafInteractionManager.cpp
@@ -32,37 +32,37 @@ mafInteractionManager::~mafInteractionManager() {
 
 void mafInteractionManager::initializeConnections() {
     // Register API signals.
-    mafRegisterLocalSignal("maf.local.resources.interaction.leftButtonPress", this, "leftButtonPressSignal()");
-    mafRegisterLocalSignal("maf.local.resources.interaction.leftButtonRelease", this, "leftButtonReleaseSignal()");
-    mafRegisterLocalSignal("maf.local.resources.interaction.middleButtonPress", this, "middleButtonPressSignal()");
-    mafRegisterLocalSignal("maf.local.resources.interaction.middleButtonRelease", this, "middleButtonReleaseSignal()");
-    mafRegisterLocalSignal("maf.local.resources.interaction.rightButtonPress", this, "rightButtonPressSignal()");
-    mafRegisterLocalSignal("maf.local.resources.interaction.rightButtonRelease", this, "rightButtonReleaseSignal()");
+    mafRegisterLocalSignal("maf.local.resources.interaction.leftButtonPress", this, "leftButtonPressSignal(unsigned long)");
+    mafRegisterLocalSignal("maf.local.resources.interaction.leftButtonRelease", this, "leftButtonReleaseSignal(unsigned long)");
+    mafRegisterLocalSignal("maf.local.resources.interaction.middleButtonPress", this, "middleButtonPressSignal(unsigned long)");
+    mafRegisterLocalSignal("maf.local.resources.interaction.middleButtonRelease", this, "middleButtonReleaseSignal(unsigned long)");
+    mafRegisterLocalSignal("maf.local.resources.interaction.rightButtonPress", this, "rightButtonPressSignal(unsigned long)");
+    mafRegisterLocalSignal("maf.local.resources.interaction.rightButtonRelease", this, "rightButtonReleaseSignal(unsigned long)");
     mafRegisterLocalSignal("maf.local.resources.interaction.enter", this, "enterSignal()");
     mafRegisterLocalSignal("maf.local.resources.interaction.leave", this, "leaveSignal()");
     mafRegisterLocalSignal("maf.local.resources.interaction.keyPress", this, "keyPressSignal()");
     mafRegisterLocalSignal("maf.local.resources.interaction.keyRelease", this, "keyReleaseSignal()");
     mafRegisterLocalSignal("maf.local.resources.interaction.keyChar", this, "charSignal()");
-    mafRegisterLocalSignal("maf.local.resources.interaction.mouseMove", this, "mouseMoveSignal()");
-    mafRegisterLocalSignal("maf.local.resources.interaction.mouseWheelForward", this, "mouseWheelForwardSignal()");
-    mafRegisterLocalSignal("maf.local.resources.interaction.mouseWheelBackward", this, "mouseWheelBackwardSignal()");
+    mafRegisterLocalSignal("maf.local.resources.interaction.mouseMove", this, "mouseMoveSignal(unsigned long)");
+    mafRegisterLocalSignal("maf.local.resources.interaction.mouseWheelForward", this, "mouseWheelForwardSignal(unsigned long)");
+    mafRegisterLocalSignal("maf.local.resources.interaction.mouseWheelBackward", this, "mouseWheelBackwardSignal(unsigned long)");
     mafRegisterLocalSignal("maf.local.resources.interaction.pick", this, "pickSignal()");
 
     // Register private callbacks.
-    mafRegisterLocalCallback("maf.local.resources.interaction.leftButtonPress", this, "leftButtonPress()");
-    mafRegisterLocalCallback("maf.local.resources.interaction.leftButtonRelease", this, "leftButtonRelease()");
-    mafRegisterLocalCallback("maf.local.resources.interaction.middleButtonPress", this, "middleButtonPress()");
-    mafRegisterLocalCallback("maf.local.resources.interaction.middleButtonRelease", this, "middleButtonRelease()");
-    mafRegisterLocalCallback("maf.local.resources.interaction.rightButtonPress", this, "rightButtonPress()");
-    mafRegisterLocalCallback("maf.local.resources.interaction.rightButtonRelease", this, "rightButtonRelease()");
+    mafRegisterLocalCallback("maf.local.resources.interaction.leftButtonPress", this, "leftButtonPress(unsigned long)");
+    mafRegisterLocalCallback("maf.local.resources.interaction.leftButtonRelease", this, "leftButtonRelease(unsigned long)");
+    mafRegisterLocalCallback("maf.local.resources.interaction.middleButtonPress", this, "middleButtonPress(unsigned long)");
+    mafRegisterLocalCallback("maf.local.resources.interaction.middleButtonRelease", this, "middleButtonRelease(unsigned long)");
+    mafRegisterLocalCallback("maf.local.resources.interaction.rightButtonPress", this, "rightButtonPress(unsigned long)");
+    mafRegisterLocalCallback("maf.local.resources.interaction.rightButtonRelease", this, "rightButtonRelease(unsigned long)");
     mafRegisterLocalCallback("maf.local.resources.interaction.enter", this, "enter()");
     mafRegisterLocalCallback("maf.local.resources.interaction.leave", this, "leave()");
     mafRegisterLocalCallback("maf.local.resources.interaction.keyPress", this, "keyPress()");
     mafRegisterLocalCallback("maf.local.resources.interaction.keyRelease", this, "keyRelease()");
     mafRegisterLocalCallback("maf.local.resources.interaction.keyChar", this, "keyChar()");
-    mafRegisterLocalCallback("maf.local.resources.interaction.mouseMove", this, "mouseMove()");
-    mafRegisterLocalCallback("maf.local.resources.interaction.mouseWheelForward", this, "mouseWheelForward()");
-    mafRegisterLocalCallback("maf.local.resources.interaction.mouseWheelBackward", this, "mouseWheelBackward()");
+    mafRegisterLocalCallback("maf.local.resources.interaction.mouseMove", this, "mouseMove(unsigned long)");
+    mafRegisterLocalCallback("maf.local.resources.interaction.mouseWheelForward", this, "mouseWheelForward(unsigned long)");
+    mafRegisterLocalCallback("maf.local.resources.interaction.mouseWheelBackward", this, "mouseWheelBackward(unsigned long)");
     mafRegisterLocalCallback("maf.local.resources.interaction.pick", this, "pick()");
 }
 
@@ -70,23 +70,23 @@ void mafInteractionManager::vmeSelected(mafVME *vme) {
     Q_UNUSED(vme);
 }
 
-void mafInteractionManager::leftButtonPress() {
+void mafInteractionManager::leftButtonPress(unsigned long modifier) {
     qDebug() << "left mouse button pressed!!";
 }
 
-void mafInteractionManager::leftButtonRelease() {
+void mafInteractionManager::leftButtonRelease(unsigned long modifier) {
 }
 
-void mafInteractionManager::middleButtonPress() {
+void mafInteractionManager::middleButtonPress(unsigned long modifier) {
 }
 
-void mafInteractionManager::middleButtonRelease() {
+void mafInteractionManager::middleButtonRelease(unsigned long modifier) {
 }
 
-void mafInteractionManager::rightButtonPress() {
+void mafInteractionManager::rightButtonPress(unsigned long modifier) {
 }
 
-void mafInteractionManager::rightButtonRelease() {
+void mafInteractionManager::rightButtonRelease(unsigned long modifier) {
 }
 
 void mafInteractionManager::enter() {
@@ -104,13 +104,13 @@ void mafInteractionManager::keyRelease() {
 void mafInteractionManager::keyChar() {
 }
 
-void mafInteractionManager::mouseMove() {
+void mafInteractionManager::mouseMove(unsigned long modifier) {
 }
 
-void mafInteractionManager::mouseWheelForward() {
+void mafInteractionManager::mouseWheelForward(unsigned long modifier) {
 }
 
-void mafInteractionManager::mouseWheelBackward() {
+void mafInteractionManager::mouseWheelBackward(unsigned long modifier) {
 }
 
 void mafInteractionManager::pick() {
