2b2d5f3bf9c52160770aa99b5ddbbbce0a4b1f3f
Daniele Giunchi <d.giunchi@scsitaly.com>
2011-05-19 22:41:43 +0200

    add rules for python

--- src/mafQA/qa/Executer.py
+++ src/mafQA/qa/Executer.py
@@ -1,4 +1,9 @@
 class Executer:
+    """Executer executes and handlse the result of a rule on a file.
+
+    It returns 1 if the rules finds an error, 0 otherwise.
+    Append the report to the output file. 
+    """
     def __init__(self):
         self.dom = None
         self.FileInput = None
