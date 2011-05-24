139be1f8fb9e1dc377b1a73417fee3b767818298
Daniele Giunchi <d.giunchi@scsitaly.com>
2011-05-24 16:46:06 +0200

    add modules for download and build OpenCV

---
+++ CMakeExternals/External_OpenCV.cmake
@@ -0,0 +1,44 @@
+#
+# OpenCV
+#
+
+SET(OpenCV_DEPENDS)
+  
+  SET(OpenCV_enabling_variable OpenCV_LIBRARY)
+  
+  SET(proj OpenCV)
+  SET(proj_DEPENDENCIES)
+  
+  SET(OpenCV_DEPENDS ${proj})
+
+  IF(NOT DEFINED OpenCV_DIR)
+    #MESSAGE("Adding project:${proj}")
+    
+    ExternalProject_Add(${proj}
+      GIT_REPOSITORY "${git_protocol}://github.com/SCS-B3C/OpenCV.git"
+      GIT_TAG "origin/master"
+      CMAKE_GENERATOR ${gen}
+      INSTALL_COMMAND ""
+      CMAKE_ARGS
+        ${ep_common_args}
+        -DBUILD_NEW_PYTHON_SUPPORT:BOOL=FALSE
+        -DBUILD_DOCUMENTATION:BOOL=FALSE
+        -DQT_QMAKE_EXECUTABLE:FILEPATH=${QT_QMAKE_EXECUTABLE}
+      DEPENDS
+        ${proj_DEPENDENCIES}
+      )
+    SET(OpenCV_DIR ${ep_build_dir}/${proj})
+    
+    # Since OpenCV is statically build, there is not need to add its corresponding 
+    # library output directory to CTK_EXTERNAL_LIBRARY_DIRS
+  
+  ELSE()
+    include(CMake/mafMacroEmptyExternalProject.cmake)
+    mafMacroEmptyExternalProject(${proj} "${proj_DEPENDENCIES}")
+  ENDIF()
+
+  SET(${OpenCV_enabling_variable}_INCLUDE_DIRS OpenCV_INCLUDE_DIRS)
+  SET(${OpenCV_enabling_variable}_FIND_PACKAGE_CMD OpenCV)
+    
+
+
