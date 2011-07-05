72f3fbf165510285c486395e776ffb4e53943a62
Daniele Giunchi <d.giunchi@scsitaly.com>
2011-07-05 15:49:42 +0200

    fix problem when network is down and it needs to build from superbuild project

--- CMakeExternals/External_OpenCV.cmake
+++ CMakeExternals/External_OpenCV.cmake
@@ -10,7 +10,39 @@ SET(OpenCV_DEPENDS)
   SET(proj_DEPENDENCIES)
   
   SET(OpenCV_DEPENDS ${proj})
+  execute_process(COMMAND git remote show "${git_protocol}://github.com/SCS-B3C/OpenCV.git"
+                  WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+                  RESULT_VARIABLE RESULT)
+                  
+  IF(RESULT)
+  MESSAGE("OpenCV can not be downloaded with git, try in local filesystem")
+  IF(NOT DEFINED OpenCV_DIR)
+    #MESSAGE("Adding project:${proj}")
+    
+    ExternalProject_Add(${proj}
+      DOWNLOAD_DIR "${ep_build_dir}/${proj}"
+      CMAKE_GENERATOR ${gen}
+      INSTALL_COMMAND ""
+      CMAKE_ARGS
+        ${ep_common_args}
+        -DBUILD_NEW_PYTHON_SUPPORT:BOOL=FALSE
+        -DBUILD_DOXYGEN_DOCS:BOOL=FALSE
+        -DQT_QMAKE_EXECUTABLE:FILEPATH=${QT_QMAKE_EXECUTABLE}
+      DEPENDS
+        ${proj_DEPENDENCIES}
+      )
+    SET(OpenCV_DIR ${ep_build_dir}/${proj})
+    
+    # Since OpenCV is statically build, there is not need to add its corresponding 
+    # library output directory to CTK_EXTERNAL_LIBRARY_DIRS
+  
+  ELSE()
+    include(CMake/mafMacroEmptyExternalProject.cmake)
+    mafMacroEmptyExternalProject(${proj} "${proj_DEPENDENCIES}")
+  ENDIF()
 
+  ELSE(RESULT)
+    MESSAGE("OpenCV will be download with git, if in future some network problem occurs, rerun Cmake for using local filesystem sources already downloaded")
   IF(NOT DEFINED OpenCV_DIR)
     #MESSAGE("Adding project:${proj}")
     
@@ -37,6 +69,7 @@ SET(OpenCV_DEPENDS)
     mafMacroEmptyExternalProject(${proj} "${proj_DEPENDENCIES}")
   ENDIF()
 
+  ENDIF(RESULT)
   SET(${OpenCV_enabling_variable}_INCLUDE_DIRS OpenCV_INCLUDE_DIRS)
   SET(${OpenCV_enabling_variable}_FIND_PACKAGE_CMD OpenCV)
     
