0d3df80c3aef54d8885401db999fdcda78a2d0c4
Daniele Giunchi <d.giunchi@scsitaly.com>
2011-07-13 13:52:40 +0200

    add pipe box and test

---
+++ src/mafPluginVTK/mafPipeVisualVTKBox.cpp
@@ -0,0 +1,68 @@
+/*
+ *  mafPipeVisualVTKBox.cpp
+ *  mafPluginVTK
+ *
+ *  Created by Paolo Quadrani on 13/07/11.
+ *  Copyright 2011 B3C. All rights reserved.
+ *
+ *  See Licence at: http://tiny.cc/QXJ4D
+ *
+ */
+
+#include "mafPipeVisualVTKBox.h"
+#include "mafVTKWidget.h"
+
+#include <vtkSmartPointer.h>
+#include <vtkAlgorithmOutput.h>
+#include <vtkOutlineFilter.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkActor.h>
+#include <vtkRendererCollection.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderer.h>
+
+using namespace mafCore;
+using namespace mafResources;
+using namespace mafPluginVTK;
+
+mafPipeVisualVTKBox::mafPipeVisualVTKBox(const QString code_location) : mafPipeVisualVTK(code_location) {
+    m_OutlineFilter = vtkOutlineFilter::New();
+
+    vtkPolyDataMapper *mapper = vtkPolyDataMapper::New();
+    mapper->SetInputConnection(0, m_OutlineFilter->GetOutputPort(0));
+    m_Prop3D = vtkActor::New();
+    m_Prop3D.setDestructionFunction(&vtkActor::Delete);
+    vtkActor::SafeDownCast(m_Prop3D)->SetMapper(mapper);
+    mapper->Delete();
+    m_Output = &m_Prop3D;
+}
+
+mafPipeVisualVTKBox::~mafPipeVisualVTKBox() {
+}
+
+bool mafPipeVisualVTKBox::acceptObject(mafCore::mafObjectBase *obj) {
+    mafVME *vme = qobject_cast<mafVME*>(obj);
+    if(vme != NULL) {
+        QString dataType = vme->dataSetCollection()->itemAtCurrentTime()->externalDataType();
+        if(dataType.contains(QRegExp("^vtk.*"))) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void mafPipeVisualVTKBox::updatePipe(double t) {
+    setModified(false);
+    mafDataSet *data = dataSetForInput(0, t);
+    if(data == NULL) {
+        return;
+    }
+
+    mafProxy<vtkAlgorithmOutput> *dataSet = mafProxyPointerTypeCast(vtkAlgorithmOutput, data->dataValue());
+    if (dataSet == NULL) {
+        m_OutlineFilter->RemoveAllInputs();
+    } else {
+        m_OutlineFilter->SetInputConnection((*dataSet));
+        m_OutlineFilter->Update();
+    }
+}
---
+++ src/mafPluginVTK/mafPipeVisualVTKBox.h
@@ -0,0 +1,56 @@
+/*
+ *  mafPipeVisualVTKBox.h
+ *  mafPluginVTK
+ *
+ *  Created by Paolo Quadrani on 13/07/11.
+ *  Copyright 2011 B3C. All rights reserved.
+ *
+ *  See Licence at: http://tiny.cc/QXJ4D
+ *
+ */
+
+#ifndef MAFVISUALPIPEVTKBOX_H
+#define MAFVISUALPIPEVTKBOX_H
+
+// Includes list
+#include "mafPluginVTKDefinitions.h"
+#include "mafPipeVisualVTK.h"
+
+// Foundation Class forwarding list
+class vtkActor;
+class vtkOutlineFilter;
+
+namespace mafPluginVTK {
+
+/**
+ Class name: mafPipeVisualVTKBox
+ This class represents visual pipe used to render the boundary of the current data.
+ This pipe will use the vtkCornerFilter to draw a box around the data to show its boundary.
+ */
+class MAFPLUGINVTKSHARED_EXPORT mafPipeVisualVTKBox : public mafPipeVisualVTK {
+    Q_OBJECT
+    /// typedef macro.
+    mafSuperclassMacro(mafPluginVTK::mafPipeVisualVTK);
+
+public:
+    /// Object constructor;
+    mafPipeVisualVTKBox(const QString code_location = "");
+
+    /// Accept function
+    static bool acceptObject(mafCore::mafObjectBase *obj);
+    
+public slots:
+    /// Allow to execute and update the pipeline when something change.
+    /*virtual*/ void updatePipe(double t = -1);
+    
+protected:
+    /// Object destructor.
+    /* virtual */ ~mafPipeVisualVTKBox();
+    
+private:
+    vtkOutlineFilter *m_OutlineFilter; ///< Filter used to generate the outline box for input data object.
+};
+
+} // namespace mafPluginVTK
+
+#endif // MAFVISUALPIPEVTKBOX
