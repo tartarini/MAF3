539887c96f43d9a097e46a5c819a0f29bd5d910b
Daniele Giunchi <d.giunchi@scsitaly.com>
2011-07-20 11:54:41 +0200

    nanogear dependencies in cmake scripts

--- src/mafEventBus/target_libraries.cmake
+++ src/mafEventBus/target_libraries.cmake
@@ -4,10 +4,17 @@
 # This file should list the libraries required to build the current MAF module.
 # 
 
+SET(nanogear_libraries
+  Nanogear
+  NanogearIHTTPConnector
+)
+
 SET(foundation_libraries 
   qxmlrpc
   QtSOAP
+  ${nanogear_libraries}
 )
+
 
 SET(target_libraries
   ${MAF_BASE_LIBRARIES}
