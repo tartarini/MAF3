a2f126ccced5bead9b65c6e10d241f667e3a2c28
Daniele Giunchi <d.giunchi@scsitaly.com>
2011-07-27 11:10:22 +0200

    extend include rule to use also a dictionary of allowed includes

--- src/mafQA/qa/Rules/ClassStructureRules/UselessIncludesRule.py
+++ src/mafQA/qa/Rules/ClassStructureRules/UselessIncludesRule.py
@@ -6,6 +6,7 @@ import os, time
 class UselessIncludesRule(AbstractRule):
     def __init__(self):
         AbstractRule.__init__(self)
+        self.DictionaryList = []
 
     def execute(self):
         self.dom = xd.parse(self.FullPathInputFile)
@@ -17,17 +18,27 @@ class UselessIncludesRule(AbstractRule):
             pass
         refInclude = self.dom.getElementsByTagName('compounddef')[0].getElementsByTagName('includes')[0].attributes["refid"].value
         
+        f = open("./Rules/ClassStructureRules/" + self.ParameterList[0], 'r')
+        lines = f.readlines()
+        for line in lines:
+            self.DictionaryList.append(line.replace("\n","").replace("\r",""))
+        
         directory =  os.path.dirname(self.FullPathInputFile)
         xmlFile = os.path.join(directory, refInclude + ".xml")
         extDom = xd.parse(xmlFile)
         
         includeList = extDom.getElementsByTagName('compounddef')[0].getElementsByTagName('includes')
         classToIcludeList = [i.firstChild.nodeValue.split('.')[0] for i in includeList]
-        print len(classToIcludeList)
+        #print len(classToIcludeList)
         dict = {}
         for item in classToIcludeList:
             dict[item] = False
-            print item
+            #print className, item
+            if(item == baseClassName or (item + ".h") in self.DictionaryList):
+                dict[item] = True
+                #print item , self.DictionaryList
+                continue
+                
             members = self.dom.getElementsByTagName('memberdef')
             for member in members:
               attrs = member.attributes
@@ -35,7 +46,7 @@ class UselessIncludesRule(AbstractRule):
                 #check type
                 typeVariable = member.getElementsByTagName('type')[0].firstChild.nodeValue
                 #check
-                if(typeVariable != None and item in typeVariable and (item + "*") in typeVariable and (item + " *") in typeVariable):
+                if(typeVariable != None and item in typeVariable and not((item + "*") in typeVariable) and not((item + " *") in typeVariable)):
                   #include correct! exit from for loop
                   dict[item] = True
                   continue
