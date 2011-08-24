10b7c8f49d6d1139c262b3d3d70955d8a1ded80c
Paolo Quadrani <paolo.quadrani@gmail.com>
2011-08-24 15:48:33 +0200

    Extracted python search part.

--- CMake/mafMacroFindWrap.cmake
+++ CMake/mafMacroFindWrap.cmake
@@ -1,9 +1,9 @@
 #
-#  mafMacroWrapTargetFiles.cmake
+#  mafMacroFindWrap.cmake
 #  maf
 #
 #  Created by Daniele Giunchi - Paolo Quadrani on 10/09/10.
-#  Copyright 2009 B3C. All rights reserved.
+#  Copyright 2011 B3C. All rights reserved.
 #
 #  See Licence at: http://tiny.cc/QXJ4D
 #
@@ -56,15 +56,4 @@ endif(SWIG_FOUND)
 ## Python
 ## #################################################################
 
-find_package(PythonLibs QUIET)
-
-if(PYTHONLIBS_FOUND)
-  set(PYTHON_DEBUG_LIBRARY "")
-  include_directories(${PYTHON_INCLUDE_PATH})
-  get_filename_component(PYTHON_PATH ${PYTHON_LIBRARIES} PATH)
-  link_directories(${PYTHON_PATH})
-endif(PYTHONLIBS_FOUND)
-
-if(PYTHONLIBS_FOUND)
-  add_definitions(-DHAVE_PYTHON)
-endif(PYTHONLIBS_FOUND)
+include(mafMacroFindPython.cmake)
