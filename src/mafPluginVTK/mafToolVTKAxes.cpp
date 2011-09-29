361a24c867783867847f466a3d419e5a780af50b
Paolo Quadrani <paolo.quadrani@gmail.com>
2011-09-29 10:18:22 +0200

    Axes tool implementation.

---
+++ src/mafPluginVTK/mafToolVTKAxes.cpp
@@ -0,0 +1,55 @@
+/*
+ *  mafToolVTKAxes.cpp
+ *  mafPluginVTK
+ *
+ *  Created by Paolo Quadrani on 27/09/11.
+ *  Copyright 2011 B3C. All rights reserved.
+ *
+ *  See License at: http://tiny.cc/QXJ4D
+ *
+ */
+
+#include "mafToolVTKAxes.h"
+#include "mafSceneNodeVTK.h"
+
+#include <vtkAssembly.h>
+#include <vtkAxesActor.h>
+
+using namespace mafPluginVTK;
+
+
+mafToolVTKAxes::mafToolVTKAxes(const QString code_location) : mafToolVTK(code_location) {
+    m_AxesActor = vtkAxesActor::New();
+    m_AxesActor->AxisLabelsOff();
+    m_AxesActor->SetPickable(0);
+    m_AxesActor->SetVisibility(0);
+}
+
+mafToolVTKAxes::~mafToolVTKAxes() {
+    m_AxesActor->Delete();
+}
+
+void mafToolVTKAxes::setSceneNode(mafResources::mafSceneNode *node) {
+    vtkAssembly *assembly = NULL;
+    if (m_SceneNode) {
+        // remove the axes from the old scene node...
+        assembly = ((mafSceneNodeVTK *)m_SceneNode)->nodeAssembly();
+        assembly->RemovePart(m_AxesActor);
+    }
+    // initialize the member variable...
+    Superclass::setSceneNode(node);
+
+    // ... then add the axes actor to the new assembly...
+    if (m_SceneNode) {
+        assembly = ((mafSceneNodeVTK *)m_SceneNode)->nodeAssembly();
+        assembly->AddPart(m_AxesActor);
+    }
+}
+
+void mafToolVTKAxes::graphicObjectInitialized() {
+    addProp(m_AxesActor);
+}
+
+void mafToolVTKAxes::updateVisibility() {
+    m_AxesActor->SetVisibility(visibility() ? 1 : 0);
+}
---
+++ src/mafPluginVTK/mafToolVTKAxes.h
@@ -0,0 +1,57 @@
+/*
+ *  mafToolVTKAxes.h
+ *  mafPluginVTK
+ *
+ *  Created by Paolo Quadrani on 27/09/11.
+ *  Copyright 2011 B3C. All rights reserved.
+ *
+ *  See License at: http://tiny.cc/QXJ4D
+ *
+ */
+
+#ifndef MAFTOOLVTKAXES_H
+#define MAFTOOLVTKAXES_H
+
+// Includes list
+#include "mafPluginVTK_global.h"
+#include "mafToolVTK.h"
+
+class vtkAxesActor;
+
+namespace mafPluginVTK {
+
+/**
+ Class name: mafToolVTKAxes
+ This is the tool representing a Cartesian axes.
+ */
+class MAFPLUGINVTKSHARED_EXPORT mafToolVTKAxes : public mafToolVTK {
+    Q_OBJECT
+    
+    /// typedef macro.
+    mafSuperclassMacro(mafPluginVTK::mafToolVTK);
+
+public Q_SLOTS:
+    /// Called when the graphic object has been initialized ready to use
+    /*virtual*/ void graphicObjectInitialized();
+
+public:
+     /// Object constructor.
+    mafToolVTKAxes(const QString code_location = "");
+
+    /// Assign the scene node to the tool so to have access to the VME and eventually its visual representation.
+    /*virtual*/ void setSceneNode(mafResources::mafSceneNode *node);
+
+protected:
+    /// Object destructor.
+    /* virtual */ ~mafToolVTKAxes();
+
+    /// update visibility for actor or volume passed as parameter
+    /*virtual*/ void updateVisibility();
+
+private:
+    vtkAxesActor *m_AxesActor; ///< Axes to show the reference system of the node.
+};
+
+} //namespace mafPluginVTK
+
+#endif // MAFTOOLVTKAXES_H
