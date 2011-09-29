22b85237ef074556d6d69d56ef1244002e54e4ce
Paolo Quadrani <paolo.quadrani@gmail.com>
2011-09-29 10:15:47 +0200

    Base class for MAF3 tools.

--- src/mafResources/mafTool.cpp
+++ src/mafResources/mafTool.cpp
@@ -5,18 +5,32 @@
  *  Created by Paolo Quadrani on 27/09/11.
  *  Copyright 2011 B3C. All rights reserved.
  *
- *  See Licence at: http://tiny.cc/QXJ4D
+ *  See License at: http://tiny.cc/QXJ4D
  *
  */
 
 #include "mafTool.h"
-#include "mafVME.h"
+#include "mafSceneNode.h"
 
 using namespace mafResources;
 
 
-mafTool::mafTool(const QString code_location) : mafPipeVisual(code_location) {
+mafTool::mafTool(const QString code_location) : mafPipeVisual(code_location), m_SceneNode(NULL) {
+    connect(this, SIGNAL(graphicObjectReady()), this, SLOT(graphicObjectInitialized()));
 }
 
 mafTool::~mafTool() {
+}
+
+void mafTool::setSceneNode(mafSceneNode *node) {
+    if (m_SceneNode != node) {
+        m_SceneNode = node;
+    }
+}
+
+void mafTool::updatePipe(double t) {
+
+}
+
+void mafTool::graphicObjectInitialized() {
 }
--- src/mafResources/mafTool.h
+++ src/mafResources/mafTool.h
@@ -5,7 +5,7 @@
  *  Created by Paolo Quadrani on 27/09/11.
  *  Copyright 2011 B3C. All rights reserved.
  *
- *  See Licence at: http://tiny.cc/QXJ4D
+ *  See License at: http://tiny.cc/QXJ4D
  *
  */
 
@@ -16,6 +16,8 @@
 #include "mafPipeVisual.h"
 
 namespace mafResources {
+
+class mafSceneNode;
 
 /**
  Class name: mafTool
@@ -28,13 +30,29 @@ class MAFRESOURCESSHARED_EXPORT mafTool : public mafPipeVisual {
     /// typedef macro.
     mafSuperclassMacro(mafResources::mafPipeVisual);
 
+Q_SIGNALS:
+    /// Emitted when the graphic object has been initialized ready to use
+    void graphicObjectReady();
+
+public Q_SLOTS:
+    /// Allow to execute and update the pipeline when something change
+    /*virtual*/ void updatePipe(double t = -1);
+
+    /// Called when the graphic object has been initialized ready to use
+    virtual void graphicObjectInitialized();
+
 public:
      /// Object constructor.
     mafTool(const QString code_location = "");
 
+    /// Assign the scene node to the tool so to have access to the VME and eventually its visual representation.
+    virtual void setSceneNode(mafSceneNode *node);
+
 protected:
     /// Object destructor.
     /* virtual */ ~mafTool();
+
+    mafSceneNode *m_SceneNode; ///< Pointer to the scene node.
 };
 
 } //namespace mafResources
