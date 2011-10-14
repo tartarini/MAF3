3064e8375f65084a0468a505b4b116912007b853
Daniele Giunchi <d.giunchi@scsitaly.com>
2011-10-14 11:54:57 +0200

    extend API and upgrade comments

--- src/mafCore/mafSQLITE.h
+++ src/mafCore/mafSQLITE.h
@@ -18,6 +18,7 @@
 
 // Class forwarding list
 class QSqlTableModel;
+class QSqlQuery;
 
 namespace mafCore {
 
@@ -45,19 +46,25 @@ public:
     /// return true if there is any connected DB.
     bool isConnected();
 
-    /// Allows to execute the query on the table model connected to the BD.
-    void setQueryTable(const QString &queryString);
-
     /// Allows to execute the query on the BD.
-    void setQueryDb(const QString &queryString);
+    /** Insert and Updates does NOT imply the submit of the changes to the Database, for submitting the changes
+     it needs to call submitAllChanges() */
+    QSqlQuery *executeQuery(const QString &queryString);
 
     /// Return the record associated to the given index. If the index is out of bound NULL is returned.
     QSqlRecord record(int idx);
 
 public Q_SLOTS:
     /// Slot that will insert into the DB table the elements contained in the hash.
+    /** Insert does NOT imply the submit of the changes to the Database, for submitting the changes
+     it needs to call submitAllChanges() */
     bool insertRecord(QHash<QString, QVariant> *recordHash);
 
+    /// Slot that will insert into the DB table the record.
+    /** Insert does NOT imply the submit of the changes to the Database, for submitting the changes
+     it needs to call submitAllChanges() */
+    bool insertRecord(const QSqlRecord &record);
+    
     /// Submit all the changes to the DB.
     void submitAllChanges();
 
@@ -71,6 +78,7 @@ protected:
 private:
     QSqlTableModel *m_TableModel; ///< Model associated to the DB table.
     bool m_DBConnected; ///< Variable containing the connection status with the database.
+    QSqlQuery *m_Query; ///< Represents the current executed query.
 };
 
 /////////////////////////////////////////////////////////////
