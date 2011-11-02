a5f1d946611e9c8a2255f0213836a295de835426
Paolo Quadrani <paolo.quadrani@gmail.com>
2011-11-02 10:23:12 +0100

    Added methods to set the view's list associated with the splitter.

--- src/mafGUI/mafSplitter.h
+++ src/mafGUI/mafSplitter.h
@@ -16,25 +16,37 @@
 #include "mafGUIDefinitions.h"
 #include <QSplitter>
 #include <QWidget>
+#include <mafObjectBase.h>
 
 /**
  Class Name: mafSplitter
- Class used to create splitted panels like OrthoSlice view.
+ Class used to create split panels like OrthoSlice view.
 */
 class MAFGUISHARED_EXPORT mafSplitter : public QSplitter {
 	Q_OBJECT
     
 public:
+    /// Object constructor
     mafSplitter(QWidget * parent = 0);
 
+    /// Allows to adjust position of the split panel
     void adjustPosition(int p);
+
+    /// Allows to assign the mafViews associated with the two panel of the splitter
+    void setViews(QList<mafCore::mafObjectBase*> &vlist);
+
+    /// Add a mafView into the views list.
+    void addView(mafCore::mafObjectBase *view);
 
 Q_SIGNALS:
     /// Signal used to ask the splitter synchronization.
     void synchronizeSplitterSignal(QObject *splitter);
 
 protected:
+    /// Redefine the paint event to allow sending the synchronize event.
     void paintEvent ( QPaintEvent * pe );
+
+    QList<mafCore::mafObjectBase*> m_Views; ///< List of mafView to use into the mafSplitter.
 };
 
 #endif // MAFSPLITTER_H
