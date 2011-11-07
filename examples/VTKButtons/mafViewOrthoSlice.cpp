7003e65101ff6ee62979de785b33590b3fd1dd28
Paolo Quadrani <paolo.quadrani@gmail.com>
2011-11-07 17:15:35 +0100

    Defined method to manage VME visualization.

--- examples/VTKButtons/mafViewOrthoSlice.cpp
+++ examples/VTKButtons/mafViewOrthoSlice.cpp
@@ -10,6 +10,7 @@
  */
 
 #include "mafViewOrthoSlice.h"
+#include <mafSceneNode.h>
 
 #include <mafEventBusManager.h>
 
@@ -32,8 +33,65 @@ bool mafViewOrthoSlice::initialize() {
         if (m_RenderWidget == NULL) {
             qCritical() << mafTr("Problem loading layout configuration file %1").arg(cf);
         }
+        extractViews(m_RenderWidget);
         setupSceneGraph();
         return m_RenderWidget != NULL;
     }
     return false;
+}
+
+void mafViewOrthoSlice::extractViews(QObject *root) {
+    QObjectList c = root->children();
+    Q_FOREACH(QObject *obj, c) {
+        if (obj->children().count() > 0) {
+            extractViews(obj);
+        }
+        mafView *v = qobject_cast<mafResources::mafView *>(obj);
+        if (v) {
+            m_ViewList.append(v);
+        }
+    }
+}
+
+void mafViewOrthoSlice::showSceneNode(mafResources::mafSceneNode *node, bool show) {
+    mafResources::mafView *subView;
+    Q_FOREACH(subView, m_ViewList) {
+        subView->showSceneNode(subView->sceneNodeFromVme((mafCore::mafObjectBase *)node->vme()), show);
+    }
+}
+
+mafResources::mafSceneNode *mafViewOrthoSlice::createSceneNode(mafResources::mafVME *vme) {
+    mafResources::mafView *subView;
+    Q_FOREACH(subView, m_ViewList) {
+        subView->createSceneNode(vme);
+    }
+    return Superclass::createSceneNode(vme);
+}
+
+void mafViewOrthoSlice::selectSceneNode(mafResources::mafSceneNode *node, bool select) {
+    mafResources::mafView *subView;
+    Q_FOREACH(subView, m_ViewList) {
+        if (node) {
+            subView->selectSceneNode(subView->sceneNodeFromVme((mafCore::mafObjectBase *)node->vme()), select);
+        } else {
+            // reset the selected node for the sub-view.
+            subView->selectSceneNode(NULL, select);
+        }
+    }
+    Superclass::selectSceneNode(node, select);
+}
+
+void mafViewOrthoSlice::removeSceneNode(mafResources::mafSceneNode *node) {
+    mafResources::mafView *subView;
+    Q_FOREACH(subView, m_ViewList) {
+        subView->removeSceneNode(subView->sceneNodeFromVme((mafCore::mafObjectBase *)node->vme()));
+    }
+    Superclass::removeSceneNode(node);
+}
+
+void mafViewOrthoSlice::updateView() {
+    mafResources::mafView *subView;
+    Q_FOREACH(subView, m_ViewList) {
+        subView->updateView();
+    }
 }
--- examples/VTKButtons/mafViewOrthoSlice.h
+++ examples/VTKButtons/mafViewOrthoSlice.h
@@ -26,11 +26,33 @@ public:
     /// Create the view
     /*virtual*/ bool initialize();
 
+    /// Remove scene node passed as argument.
+    /*virtual*/ void removeSceneNode(mafResources::mafSceneNode *node);
+
+    /// Show scene node passed as argument.
+    /*virtual*/ void showSceneNode(mafResources::mafSceneNode *node, bool show = true);
+
+    /// Select a scene node.
+    /*virtual*/ void selectSceneNode(mafResources::mafSceneNode *node, bool select);
+
+    /// factory method for creating SceneNode VTK.
+    /*virtual*/ mafResources::mafSceneNode *createSceneNode(mafResources::mafVME *vme);
+
 protected:
     /// Object destructor.
     /* virtual */ ~mafViewOrthoSlice();
 
+private:
+    /// Extract all the mafView present in the widget's hierarchy.
+    void extractViews(QObject *root);
+
+    QList<mafResources::mafView *> m_ViewList; ///< List of child views.
+
 Q_SIGNALS:
     /// Signal used to propagate the mafSceneNode visibility.
     void propagateSceneNodeShowSignale();
+
+public Q_SLOTS:
+    /// Update sub-views.
+    /*virtual*/ void updateView();
 };
