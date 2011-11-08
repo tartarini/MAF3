6aec507c163c419b2a863ae978e6c1de488ab4c0
Paolo Quadrani <paolo.quadrani@gmail.com>
2011-11-08 12:36:48 +0100

    Base class for compound views.

---
+++ src/mafResources/mafViewCompound.h
@@ -0,0 +1,54 @@
+/*
+ *  mafViewCompound.h
+ *  VTKButtons
+ *
+ *  Created by Paolo Quadrani on 27/10/11.
+ *  Copyright 2011 B3C. All rights reserved.
+ *
+ *  See License at: http://tiny.cc/QXJ4D
+ *
+ */
+
+#ifndef MAFVIEWCOMPOUND_H
+#define MAFVIEWCOMPOUND_H
+
+#include "mafView.h"
+
+namespace mafResources {
+
+/**
+ Class name: mafViewCompound
+ Base class for compound views.
+*/
+class MAFRESOURCESSHARED_EXPORT mafViewCompound : public mafView {
+    Q_OBJECT
+    mafSuperclassMacro(mafResources::mafView);
+    
+public:
+    /// Object constructor.
+    mafViewCompound(const QString code_location = "");
+    
+    /// Create the view
+    /*virtual*/ bool initialize();
+
+    /// Show scene node passed as argument.
+    /*virtual*/ void showSceneNode(mafSceneNode *node, bool show = true);
+
+protected:
+    /// Object destructor.
+    /* virtual */ ~mafViewCompound();
+
+private:
+    /// Extract all the mafView present in the widget's hierarchy.
+    void extractViews(QObject *root);
+
+    QList<mafView *> m_ViewList; ///< List of child views.
+
+public Q_SLOTS:
+    /// Update sub-views.
+    /*virtual*/ void updateView();
+};
+
+} // namespace mafResources
+
+#endif // MAFVIEWCOMPOUND_H
