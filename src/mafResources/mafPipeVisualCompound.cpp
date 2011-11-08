bc5fba4d1345a3dd0e8fe5512b5cd9c569c698dc
Paolo Quadrani <paolo.quadrani@gmail.com>
2011-11-08 11:45:34 +0100

    Default visual pipe for compound views.

---
+++ src/mafResources/mafPipeVisualCompound.cpp
@@ -0,0 +1,25 @@
+/*
+ *  mafPipeVisualCompound.cpp
+ *  mafResources
+ *
+ *  Created by Paolo Quadrani on 08/11/11.
+ *  Copyright 2011 B3C. All rights reserved.
+ *
+ *  See License at: http://tiny.cc/QXJ4D
+ *
+ */
+
+#include "mafPipeVisualCompound.h"
+
+using namespace mafResources;
+
+
+mafPipeVisualCompound::mafPipeVisualCompound(const QString code_location) : mafPipeVisual(code_location) {
+}
+
+mafPipeVisualCompound::~mafPipeVisualCompound() {
+}
+
+void mafPipeVisualCompound::updatePipe(double t) {
+    PRINT_FUNCTION_NAME_INFORMATION
+}
