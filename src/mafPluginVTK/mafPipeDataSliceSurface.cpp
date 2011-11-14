dd935bfac48af9bf320fa82f4b2031c8ae5bfbd1
Paolo Quadrani <paolo.quadrani@gmail.com>
2011-11-14 12:07:20 +0100

    Data pipe to slice surfaces.

---
+++ src/mafPluginVTK/mafPipeDataSliceSurface.cpp
@@ -0,0 +1,76 @@
+/*
+ *  mafPipeDataSliceSurface.cpp
+ *  mafPluginVTK
+ *
+ *  Created by Paolo Quadrani on 14/11/11.
+ *  Copyright 2011 B3C. All rights reserved.
+ *
+ *  See License at: http://tiny.cc/QXJ4D
+ *
+ */
+
+#include "mafPipeDataSliceSurface.h"
+#include <mafVME.h>
+#include <mafDataSet.h>
+
+#include <vtkDataSet.h>
+#include <vtkPlane.h>
+#include <vtkCutter.h>
+
+
+using namespace mafCore;
+using namespace mafResources;
+using namespace mafPluginVTK;
+
+mafPipeDataSliceSurface::mafPipeDataSliceSurface(const QString code_location) : mafPipeData(code_location) {
+    m_SliceOrigin[0] = m_SliceOrigin[1] = m_SliceOrigin[2] = 0.;
+
+    m_Normal[0] = m_Normal[1] = 0.;
+    m_Normal[2] = 1.;
+
+    m_Plane = vtkSmartPointer<vtkPlane>::New();
+    m_Cutter = vtkSmartPointer<vtkCutter>::New();
+    m_Cutter->SetCutFunction(m_Plane);
+
+}
+
+mafPipeDataSliceSurface::~mafPipeDataSliceSurface() {
+}
+
+bool mafPipeDataSliceSurface::acceptObject(mafCore::mafObjectBase *obj) {
+    mafVME *vme = qobject_cast<mafVME*>(obj);
+    if(vme != NULL) {
+        QString dataType = vme->dataSetCollection()->itemAtCurrentTime()->externalDataType();
+        if(dataType == "vtkPolyData") {
+            return true;
+        }
+    }
+    return false;
+}
+
+void mafPipeDataSliceSurface::updatePipe(double t) {
+    if (inputList()->size() == 0) {
+        return;
+    }
+
+    mafDataSet *inputDataSet = dataSetForInput(0, t);
+    if(inputDataSet == NULL) {
+        return;
+    }
+
+    m_Plane->SetOrigin(m_SliceOrigin);
+    m_Plane->SetNormal(m_Normal);
+
+    //Get data contained in the mafProxy
+    mafProxy<vtkAlgorithmOutput> *surface = mafProxyPointerTypeCast(vtkAlgorithmOutput, inputDataSet->dataValue());
+
+    m_Cutter->SetInputConnection((*surface));
+    m_Cutter->Update();
+
+    if(m_OutputValue == NULL) {
+        m_OutputValue = m_Cutter->GetOutput();
+    }
+
+    output(t)->dataSetCollection()->itemAt(t)->setDataValue(&m_OutputValue);
+    Superclass::updatePipe(t);
+}
---
+++ src/mafPluginVTK/mafPipeDataSliceSurface.h
@@ -0,0 +1,106 @@
+/*
+ *  mafPipeDataSliceSurface.h
+ *  mafPluginVTK
+ *
+ *  Created by Paolo Quadrani on 14/11/11.
+ *  Copyright 2011 B3C. All rights reserved.
+ *
+ *  See License at: http://tiny.cc/QXJ4D
+ *
+ */
+
+#ifndef MAFPIPEDATASLICESURFACE_H
+#define MAFPIPEDATASLICESURFACE_H
+
+// Includes list
+#include "mafPluginVTKDefinitions.h"
+#include <mafPipeData.h>
+#include <mafProxy.h>
+#include <vtkSmartPointer.h>
+#include <vtkPolyData.h>
+
+// Foundation Class forwarding list
+class vtkPlane;
+class vtkCutter;
+
+namespace mafPluginVTK {
+
+// Class forwarding list
+
+/**
+ Class name: mafPipeDataSliceSurface
+ This class allows you to make a slice on input vtkPolyData.
+ */
+class MAFPLUGINVTKSHARED_EXPORT mafPipeDataSliceSurface : public mafResources::mafPipeData {
+    Q_OBJECT
+
+    /// typedef macro.
+    mafSuperclassMacro(mafResources::mafPipeData);
+
+public:
+    /// Object constructor.
+    mafPipeDataSliceSurface(const QString code_location = "");
+
+    /// Accept function
+    static bool acceptObject(mafCore::mafObjectBase *obj);
+
+    /// Set the origin of the slice
+    void setSliceOrigin(double *origin);
+
+    /// Return the slice origin.
+    double *sliceOrigin();
+
+    /// Set the normal of the slice
+    void setNormal(double *normal);
+
+    /// Return the slice normal.
+    double *normal();
+
+public Q_SLOTS:
+    /// Allow to execute and update the pipeline when something change
+    /*virtual*/ void updatePipe(double t = -1);
+
+protected:
+    /// Object destructor.
+    /* virtual */ ~mafPipeDataSliceSurface();
+
+private:
+    double m_SliceOrigin[3]; ///< Origin of the slice.
+    double m_Normal[3]; ///< Normal vector of the slice.
+
+	vtkSmartPointer<vtkPlane>  m_Plane;  ///< Cutting plane implicit function.
+    vtkSmartPointer<vtkCutter> m_Cutter; ///< Cutter fo the input vtkPolyData.
+    mafCore::mafProxy<vtkPolyData> m_OutputValue; ///< Output value coming from the elaboration data pipe.
+};
+
+/////////////////////////////////////////////////////////////
+// Inline methods
+/////////////////////////////////////////////////////////////
+
+inline void mafPipeDataSliceSurface::setSliceOrigin(double *origin) {
+    REQUIRE(origin);
+
+    m_SliceOrigin[0] = origin[0];
+    m_SliceOrigin[1] = origin[1];
+    m_SliceOrigin[2] = origin[2];
+}
+
+inline double *mafPipeDataSliceSurface::sliceOrigin() {
+    return m_SliceOrigin;
+}
+
+inline void mafPipeDataSliceSurface::setNormal(double *normal) {
+    REQUIRE(normal);
+
+    m_Normal[0] = normal[0];
+    m_Normal[1] = normal[1];
+    m_Normal[2] = normal[2];
+}
+
+inline double *mafPipeDataSliceSurface::normal() {
+    return m_Normal;
+}
+
+} // namespace mafPluginVTK
+
+#endif // MAFPIPEDATASLICESURFACE_H
