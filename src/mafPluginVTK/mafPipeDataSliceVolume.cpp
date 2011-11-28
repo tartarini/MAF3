f739fdd2d524e4d3d6010c62d97fda95716a8873
Paolo Quadrani <paolo.quadrani@gmail.com>
2011-11-28 12:32:19 +0100

    Updated to use the new implementation of the vtkMAFVolumeSlicer.

--- src/mafPluginVTK/mafPipeDataSliceVolume.cpp
+++ src/mafPluginVTK/mafPipeDataSliceVolume.cpp
@@ -20,32 +20,23 @@
 #include <vtkExecutive.h>
 #include <vtkPointData.h>
 
-#define TEXTURE_RES 512
-
 using namespace mafCore;
 using namespace mafResources;
 using namespace mafPluginVTK;
 
-mafPipeDataSliceVolume::mafPipeDataSliceVolume(const QString code_location) : mafPipeData(code_location), m_Image(NULL) {
+mafPipeDataSliceVolume::mafPipeDataSliceVolume(const QString code_location) : mafPipeData(code_location) {
     m_SliceOrigin[0] = m_SliceOrigin[1] = m_SliceOrigin[2] = 0.;
 
     m_Normal[0] = m_Normal[1] = 0.;
     m_Normal[2] = 1.;
 
-    m_SlicePolydata = vtkPolyData::New();
+    m_Slicer = vtkSmartPointer<vtkMAFVolumeSlicer>::New();
 
-    m_SlicerImage = vtkSmartPointer<vtkMAFVolumeSlicer>::New();
-    m_SlicerPolygonal = vtkSmartPointer<vtkMAFVolumeSlicer>::New();
-    m_SlicerPolygonal->SetOutput(m_SlicePolydata);
-
-    m_OutputValue = m_SlicerPolygonal->GetOutputPort();
+    m_OutputPolygonal = m_Slicer->GetOutputPort();
+    m_OutputTexture   = m_Slicer->GetTexturedOutputPort();
 }
 
 mafPipeDataSliceVolume::~mafPipeDataSliceVolume() {
-    m_SlicePolydata->Delete();
-    if (m_Image) {
-        m_Image->Delete();
-    }
 }
 
 bool mafPipeDataSliceVolume::acceptObject(mafCore::mafObjectBase *obj) {
@@ -60,7 +51,7 @@ bool mafPipeDataSliceVolume::acceptObject(mafCore::mafObjectBase *obj) {
 }
 
 void mafPipeDataSliceVolume::updatePipe(double t) {
-    /*if (inputList()->size() == 0) {
+    if (inputList()->size() == 0) {
         return;
     }
 
@@ -71,38 +62,17 @@ void mafPipeDataSliceVolume::updatePipe(double t) {
 
     //Get data contained in the mafProxy
     mafProxy<vtkAlgorithmOutput> *volume = mafProxyPointerTypeCast(vtkAlgorithmOutput, inputDataSet->dataValue());
-    vtkDataSet *vtk_data = vtkDataSet::SafeDownCast((*volume)->GetProducer()->GetExecutive()->GetOutputData(0));
 
-    m_SlicerImage->SetInputConnection(*volume);
-    m_SlicerPolygonal->SetInputConnection(*volume);
-
-    double spc[3] = {.33, .33, 1.};
-    if(vtk_data->IsA("vtkImageData") || vtk_data->IsA("vtkStructuredPoints")) {
-        ((vtkImageData *)vtk_data)->GetSpacing(spc);
-    }
-
-    if (m_Image == NULL) {
-        m_Image = vtkImageData::New();
-        m_Image->SetScalarType(vtk_data->GetPointData()->GetScalars()->GetDataType());
-        m_Image->SetExtent(0, TEXTURE_RES - 1, 0, TEXTURE_RES - 1, 0, 0);
-        m_Image->SetSpacing(spc);
-        m_SlicerImage->SetOutput(m_Image);
-        m_SlicerPolygonal->SetTexture(m_Image);
-    }
-
-    m_SlicerImage->SetPlaneOrigin(m_SliceOrigin);
-    m_SlicerPolygonal->SetPlaneOrigin(m_SliceOrigin);
+    m_Slicer->SetInputConnection(*volume);
+    m_Slicer->SetPlaneOrigin(m_SliceOrigin);
 
     float m_XVector[3] = {1., 0., 0.};
     float m_YVector[3] = {0., 1., 0.};
-    m_SlicerImage->SetPlaneAxisX(m_XVector);
-    m_SlicerImage->SetPlaneAxisY(m_YVector);
-    m_SlicerPolygonal->SetPlaneAxisX(m_XVector);
-    m_SlicerPolygonal->SetPlaneAxisY(m_YVector);
+    m_Slicer->SetPlaneAxisX(m_XVector);
+    m_Slicer->SetPlaneAxisY(m_YVector);
 
-    m_SlicerImage->Update();
-    m_SlicerPolygonal->Update();
+    m_Slicer->Update();
 
-    output(t)->dataSetCollection()->itemAt(t)->setDataValue(&m_OutputValue);
-    Superclass::updatePipe(t);*/
+    output(t)->dataSetCollection()->itemAt(t)->setDataValue(&m_OutputPolygonal);
+    Superclass::updatePipe(t);
 }
