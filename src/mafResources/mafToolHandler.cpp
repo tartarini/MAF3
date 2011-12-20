0a225cc2dc1670a47b6ad601e7009711513676f7
Paolo Quadrani <paolo.quadrani@gmail.com>
2011-12-20 16:38:42 +0100

    Extended documentation, implemented method to keep updated the tools visibility, active SceneNode and graphicObject.

--- src/mafResources/mafToolHandler.cpp
+++ src/mafResources/mafToolHandler.cpp
@@ -11,21 +11,29 @@
 
 #include "mafToolHandler.h"
 #include "mafTool.h"
+#include "mafSceneNode.h"
+
+#include <mafEventBusManager.h>
+
 
 using namespace mafResources;
+using namespace mafEventBus;
 
-mafToolHandler::mafToolHandler(const QString code_location) : mafCore::mafObjectBase(code_location) {
+mafToolHandler::mafToolHandler(const QString code_location) : mafCore::mafObjectBase(code_location), m_SceneNode(NULL), m_GraphicObject(NULL) {
 }
 
 mafToolHandler::~mafToolHandler() {
     removeAllTools();
 }
 
-void mafToolHandler::addTool(mafTool *tool, bool follow_selection) {
+void mafToolHandler::addTool(mafTool *tool) {
     if (m_ToolList.contains(tool)) {
         return;
     }
     tool->retain();
+    if (m_GraphicObject) {
+        tool->setGraphicObject(m_GraphicObject);
+    }
     m_ToolList.append(tool);
 }
 
@@ -41,4 +49,24 @@ void mafToolHandler::removeAllTools() {
         tool->release();
     }
     m_ToolList.clear();
+}
+
+void mafToolHandler::setActiveSceneNode(mafSceneNode *node) {
+    if (node == m_SceneNode) {
+        return;
+    }
+    m_SceneNode = node;
+    Q_FOREACH(mafTool *tool, m_ToolList) {
+        if (tool->followSelectedObject()) {
+            tool->setSceneNode(m_SceneNode);
+            bool v = m_SceneNode && m_SceneNode->property("visibility").toBool();
+            tool->setVisibility(v);
+        }
+    }
+}
+
+void mafToolHandler::setVisibility(bool visible) {
+    Q_FOREACH(mafTool *tool, m_ToolList) {
+        tool->setVisibility(visible);
+    }
 }
--- src/mafResources/mafToolHandler.h
+++ src/mafResources/mafToolHandler.h
@@ -19,13 +19,17 @@
 
 namespace mafResources {
 
-// Class forwarding
+// mafResources module class forwarding
 class mafTool;
-
+class mafSceneNode;
 
 /**
  Class name: mafToolHandler
- This is the class that is in charge to manage the MAF tools.
+ This is the class that is in charge to manage the MAF3 tools.
+ To use this class, simply: 
+ - Instantiate it.
+ - Assign to it the graphic object which contains the layer in which the tools are visualized. 
+ - Add the tools that you want to be handled and then when the active SceneNode is set, the handler update the visibility of the tools that have the followSceneNode flag active.
  */
 class MAFRESOURCESSHARED_EXPORT mafToolHandler : public mafCore::mafObjectBase {
     Q_OBJECT
@@ -38,7 +42,7 @@ public:
     mafToolHandler(const QString code_location = "");
 
     /// Add a tool to be handled
-    void addTool(mafTool *tool, bool follow_selection = true);
+    void addTool(mafTool *tool);
 
     /// Return the list of added tools
     QList<mafTool *> *toolList();
@@ -49,12 +53,23 @@ public:
     /// Remove all the tools from the handler.
     void removeAllTools();
 
+    /// Assign the active SceneNode.
+    virtual void setActiveSceneNode(mafSceneNode *node);
+
+    /// Set the visibility of the tool
+    void setVisibility(bool visible = true);
+
+    /// Assign the graphic object containing the rendering layer in which visualize the tools.
+    void setGraphicObject(QObject *graphicObject);
+
 protected:
     /// Object destructor.
     /* virtual */ ~mafToolHandler();
 
 private:
-    QList<mafTool *> m_ToolList; ///< List of added tools.
+    QList<mafTool *> m_ToolList;  ///< List of added tools.
+    mafSceneNode    *m_SceneNode; ///< Last active SceneNode.
+    QObject         *m_GraphicObject;  ///< Graphic object containing the renderer associated to the tool layer.
 };
 
 /////////////////////////////////////////////////////////////
@@ -64,6 +79,12 @@ private:
 inline QList<mafTool *> *mafToolHandler::toolList() {
     return &m_ToolList;
 }
+
+inline void mafToolHandler::setGraphicObject(QObject *graphicObject) {
+    REQUIRE(graphicObject);
+    m_GraphicObject = graphicObject;
+}
+
 
 } //namespace mafResources
 
