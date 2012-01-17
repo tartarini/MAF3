ce8dca75d56c24c56ffffd2aacf2d452dce52bb6
Paolo Quadrani <paolo.quadrani@gmail.com>
2012-01-17 16:19:40 +0100

    Changed representation into textured button.

--- examples/VTKButtons/mafToolVTKButtons.cpp
+++ examples/VTKButtons/mafToolVTKButtons.cpp
@@ -17,8 +17,9 @@
 #include <vtkPNGReader.h>
 
 #include <vtkButtonWidget.h>
+#include <vtkEllipticalButtonSource.h>
 #include <vtkTexturedButtonRepresentation.h>
-#include <vtkTexturedButtonRepresentation2D.h>
+//#include <vtkTexturedButtonRepresentation2D.h>
 
 #include <vtkCommand.h>
 
@@ -63,14 +64,31 @@ mafToolVTKButtons::mafToolVTKButtons(const QString code_location) : mafPluginVTK
     image2->SetFileName(ba.data());
     image2->Update();
 
-    VTK_CREATE(vtkTexturedButtonRepresentation2D, rep);
+//     VTK_CREATE(vtkTexturedButtonRepresentation2D, rep);
+//     rep->SetNumberOfStates(2);
+//     rep->SetButtonTexture(0,image1->GetOutput());
+//     rep->SetButtonTexture(1,image2->GetOutput());
+//     rep->SetPlaceFactor(1);
+//     double bds[6]; 
+//     bds[0] = 25; bds[1] = 65; bds[2] = 50; bds[3] = 200;
+//     rep->PlaceWidget(bds);
+
+    VTK_CREATE(vtkEllipticalButtonSource, button);
+    button->TwoSidedOn();
+    button->SetCircumferentialResolution(24);
+    button->SetShoulderResolution(24);
+    button->SetTextureResolution(24);
+
+    VTK_CREATE(vtkTexturedButtonRepresentation, rep);
     rep->SetNumberOfStates(2);
     rep->SetButtonTexture(0,image1->GetOutput());
     rep->SetButtonTexture(1,image2->GetOutput());
+    rep->SetButtonGeometryConnection(button->GetOutputPort());
     rep->SetPlaceFactor(1);
-    double bds[6]; 
-    bds[0] = 25; bds[1] = 65; bds[2] = 50; bds[3] = 200;
+    double bds[6];
+    bds[0] = 0.6; bds[1] = 0.75; bds[2] = 0.6; bds[3] = 0.75; bds[4] = 0.6; bds[5] = 0.75;
     rep->PlaceWidget(bds);
+    rep->FollowCameraOn();
 
     VTK_CREATE(vtkButtonCallback, myCallback);
 
