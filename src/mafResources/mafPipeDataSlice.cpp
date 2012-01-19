532e86779d114ac96b84725740bbfaa52303dafc
Paolo Quadrani <paolo.quadrani@gmail.com>
2012-01-19 17:16:24 +0100

    Base class for pipe data slice.

---
+++ src/mafResources/mafPipeDataSlice.cpp
@@ -0,0 +1,103 @@
+/*
+ *  mafPipeDataSlice.cpp
+ *  mafResources
+ *
+ *  Created by Paolo Quadrani on 18/1/12.
+ *  Copyright 2012 B3C. All rights reserved.
+ *
+ *  See License at: http://tiny.cc/QXJ4D
+ *
+ */
+
+#include "mafPipeDataSlice.h"
+#include "mafVME.h"
+#include "mafDataSet.h"
+
+using namespace mafCore;
+using namespace mafResources;
+
+
+mafPipeDataSlice::mafPipeDataSlice(const QString code_location) : mafPipeData(code_location) {
+    m_SliceOrigin[0] = m_SliceOrigin[1] = m_SliceOrigin[2] = 0.;
+
+    this->setPlaneNormalAxes(mafPlaneNormalZ);
+}
+
+mafPipeDataSlice::~mafPipeDataSlice() {
+}
+
+void mafPipeDataSlice::updatePipe(double t) {
+    Superclass::updatePipe(t);
+}
+
+void mafPipeDataSlice::pushSlice(double distance) {
+    if (distance != 0) {
+        for (int i=0; i < 3; ++i) {
+            this->m_SliceOrigin[i] += distance * this->m_Normal[i];
+        }
+        this->updatePipe();
+    }
+}
+
+void mafPipeDataSlice::setNormal(double *normal) {
+    REQUIRE(normal);
+
+    m_Normal[0] = normal[0];
+    m_Normal[1] = normal[1];
+    m_Normal[2] = normal[2];
+
+    m_PlaneNormalAxes = mafPlaneNormalArbitrary;
+
+/*    double res[3], n;
+    double xyz[3][3] = {{1., 0., 0.}, {0., 1., 0.}, {0., 0., 1.}};
+    for (int a = 0; a < 3; ++a) {
+        vtkMath::Cross(m_Normal, xyz[a], res);
+        n = vtkMath::Norm(res);
+        if (n == 0) {
+            m_PlaneNormalAxes = (mafPlaneNormal)a;
+            break;
+        }
+    }*/
+}
+
+void mafPipeDataSlice::setPlaneNormalAxes(mafPlaneNormal axes) {
+    m_PlaneNormalAxes = axes;
+    switch (axes) {
+        case mafPlaneNormalX:
+            m_XVector[0] = 0.0001; //this is a ancestor bug... very sad.
+            m_XVector[1] = 1.;
+            m_XVector[2] = 0.;
+            m_YVector[0] = 0.;
+            m_YVector[1] = 0.;
+            m_YVector[2] = 1.;
+            m_Normal[0] = 1.;
+            m_Normal[1] = 0.;
+            m_Normal[2] = 0.;
+            break;
+        case mafPlaneNormalY:
+            m_XVector[0] = 0.;
+            m_XVector[1] = 0.;
+            m_XVector[2] = 1.;
+            m_YVector[0] = 1.;
+            m_YVector[1] = 0.;
+            m_YVector[2] = 0.;
+            m_Normal[0] = 0.;
+            m_Normal[1] = 1.;
+            m_Normal[2] = 0.;
+            break;
+        case mafPlaneNormalZ:
+            m_XVector[0] = 1.;
+            m_XVector[1] = 0.;
+            m_XVector[2] = 0.;
+            m_YVector[0] = 0.;
+            m_YVector[1] = 1.;
+            m_YVector[2] = 0.;
+            m_Normal[0] = 0.;
+            m_Normal[1] = 0.;
+            m_Normal[2] = 1.;
+            break;
+        default:
+            qDebug() << mafTr("Arbitrary slicing...");
+            break;
+    }
+}
---
+++ src/mafResources/mafPipeDataSlice.h
@@ -0,0 +1,136 @@
+/*
+ *  mafPipeDataSlice.h
+ *  mafResources
+ *
+ *  Created by Paolo Quadrani on 18/1/12.
+ *  Copyright 2012 B3C. All rights reserved.
+ *
+ *  See License at: http://tiny.cc/QXJ4D
+ *
+ */
+
+#ifndef MAFPIPEDATASLICE_H
+#define MAFPIPEDATASLICE_H
+
+// Includes list
+#include "mafResourcesDefinitions.h"
+#include "mafPipeData.h"
+
+// Foundation Class forwarding list
+
+
+namespace mafResources {
+
+typedef enum {
+    mafPlaneNormalX = 0,
+    mafPlaneNormalY,
+    mafPlaneNormalZ,
+    mafPlaneNormalArbitrary
+} mafPlaneNormal;
+
+
+// Class forwarding list
+
+/**
+ Class name: mafPipeDataSlice
+ This is the base class for slicing strategy pattern.
+ */
+class MAFRESOURCESSHARED_EXPORT mafPipeDataSlice : public mafPipeData {
+    Q_OBJECT
+
+    /// typedef macro.
+    mafSuperclassMacro(mafResources::mafPipeData);
+
+public Q_SLOTS:
+    /// Allow to execute and update the pipeline when something change
+    /*virtual*/ void updatePipe(double t = -1);
+
+public:
+    /// Object constructor.
+    mafPipeDataSlice(const QString code_location = "");
+
+    /// Set the origin of the slice
+    void setSliceOrigin(double *origin);
+
+    /// Set the origin of the slice
+    void setSliceOrigin(double origX, double origY, double origZ);
+
+    /// Return the slice origin.
+    double *sliceOrigin();
+
+    /// Set the normal of the slice
+    void setNormal(double *normal);
+
+    /// Return the slice normal.
+    double *normal();
+
+    /// Return the plane normal axes.
+    mafPlaneNormal planeNormalAxes() const;
+
+    /// Assign the plane normal axes.
+    void setPlaneNormalAxes(mafPlaneNormal axes);
+
+    /// Allows to move the slice along its normal of 'distance' amount.
+    void pushSlice(double distance);
+
+protected:
+    /// Object destructor.
+    /* virtual */ ~mafPipeDataSlice();
+
+    /// Return the X vector that defines the slicing plane.
+    const double *xVector() const;
+
+    /// Return the Y vector that defines the slicing plane.
+    const double *yVector() const;
+
+private:
+    double m_SliceOrigin[3]; ///< Origin of the slice.
+    double m_Normal[3]; ///< Normal vector of the slice.
+    double m_XVector[3];
+    double m_YVector[3];
+
+    mafPlaneNormal m_PlaneNormalAxes; ///< Constant indicating the axes on which lies the plane normal.
+};
+
+
+/////////////////////////////////////////////////////////////
+// Inline methods
+/////////////////////////////////////////////////////////////
+
+inline void mafPipeDataSlice::setSliceOrigin(double *origin) {
+    REQUIRE(origin);
+
+    m_SliceOrigin[0] = origin[0];
+    m_SliceOrigin[1] = origin[1];
+    m_SliceOrigin[2] = origin[2];
+}
+
+inline void mafPipeDataSlice::setSliceOrigin(double origX, double origY, double origZ) {
+    m_SliceOrigin[0] = origX;
+    m_SliceOrigin[1] = origY;
+    m_SliceOrigin[2] = origZ;
+}
+
+inline double *mafPipeDataSlice::sliceOrigin() {
+    return m_SliceOrigin;
+}
+
+inline double *mafPipeDataSlice::normal() {
+    return m_Normal;
+}
+
+inline mafPlaneNormal mafPipeDataSlice::planeNormalAxes() const {
+    return m_PlaneNormalAxes;
+}
+
+inline const double *mafPipeDataSlice::xVector() const {
+    return &m_XVector[0];
+}
+
+inline const double *mafPipeDataSlice::yVector() const {
+    return &m_YVector[0];
+}
+
+} // namespace mafResources
+
+#endif // MAFPIPEDATASLICE_H
