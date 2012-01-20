f9d3a65d2958c28ab640b20807b7de36b5ae4d41
Daniele Giunchi <d.giunchi@scsitaly.com>
2012-01-20 14:07:44 +0100

    set variable name instead of fixed one

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -254,7 +254,7 @@ ExternalProject_Add(${proj}
   
   #set(tmp_dir)
   #ExternalProject_Get_Property(${proj} source_dir binary_dir tmp_dir)
-  #add_custom_command(TARGET VPHOP PRE_BUILD
+  #add_custom_command(TARGET ${proj} PRE_BUILD
   #      COMMAND ${CMAKE_COMMAND} -E touch "${tmp_dir}/${proj}-cfgcmd.txt" )
   
 #message (".......................... Exiting ${CMAKE_CURRENT_LIST_FILE} ............................")
