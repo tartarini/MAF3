c7216b1af55619cdc7638cadbbb51c0af8e57c2b
Paolo Quadrani <paolo.quadrani@gmail.com>
2012-02-03 11:38:53 +0100

    Updated the notify event to get parameters and return value from the event itself and leaved the compatibility with previous mechanism.

--- src/mafEventBus/mafEventDispatcherLocal.cpp
+++ src/mafEventBus/mafEventDispatcherLocal.cpp
@@ -38,60 +38,72 @@ void mafEventDispatcherLocal::notifyEvent(const mafEvent &event_dictionary, mafE
     mafEventItemListType items = signalItemProperty(topic);
     mafEvent *itemEventProp;
 
-
+    mafEventArgumentsListPointer argListPointer = event_dictionary.argumentList();
+    bool hasArgs(argListPointer->count() > 0);
+    if (hasArgs) {
+        // We have arguments attached to the event...
+        argList = argListPointer;
+    }
+    const QGenericReturnArgument *retVal = event_dictionary.returnValue();
+    if (retVal && retVal->data() != NULL) {
+        returnArg = (QGenericReturnArgument *)retVal;
+    }
+    
+    Qt::ConnectionType connType = event_dictionary.isSynchronous() ? Qt::DirectConnection : Qt::AutoConnection;
 
     Q_FOREACH(itemEventProp, items) {
         if((*itemEventProp)[SIGNATURE].toString().length() != 0) {
             QString signal_to_emit = (*itemEventProp)[SIGNATURE].toString().split("(")[0];
             QObject *obj = (*itemEventProp)[OBJECT].value<QObject *>();
             if(argList != NULL) {
+                qDebug() << argList;
                 if (returnArg == NULL || returnArg->data() == NULL) { //don't use return value
                     switch (argList->count()) {
                         case 0:
-                            this->metaObject()->invokeMethod(obj, signal_to_emit.toAscii(), Qt::DirectConnection);
+                            this->metaObject()->invokeMethod(obj, signal_to_emit.toAscii(), connType);
                             break;
                         case 1:
                             this->metaObject()->invokeMethod(obj, signal_to_emit.toAscii(), \
-                            Qt::DirectConnection, argList->at(0));
+                            connType, argList->at(0));
                             break;
                         case 2:
                             this->metaObject()->invokeMethod(obj, signal_to_emit.toAscii(), \
-                            Qt::DirectConnection, argList->at(0), argList->at(1));
+                            connType, argList->at(0), argList->at(1));
                             break;
                         case 3:
                             this->metaObject()->invokeMethod(obj, signal_to_emit.toAscii(), \
-                            Qt::DirectConnection, argList->at(0), argList->at(1), argList->at(2));
+                            connType, argList->at(0), argList->at(1), argList->at(2));
                             break;
                         case 4:
                             this->metaObject()->invokeMethod(obj, signal_to_emit.toAscii(), \
-                             Qt::DirectConnection, argList->at(0), argList->at(1), argList->at(2), argList->at(3));
+                             connType, argList->at(0), argList->at(1), argList->at(2), argList->at(3));
                             break;
                         case 5:
                             this->metaObject()->invokeMethod(obj, signal_to_emit.toAscii(), \
-                             Qt::DirectConnection, argList->at(0), argList->at(1), argList->at(2), argList->at(3), argList->at(4));
+                             connType, argList->at(0), argList->at(1), argList->at(2), argList->at(3), argList->at(4));
                             break;
                         case 6:
                             this->metaObject()->invokeMethod(obj, signal_to_emit.toAscii(), \
-                             Qt::DirectConnection, argList->at(0), argList->at(1), argList->at(2), argList->at(3), argList->at(4), argList->at(5));
+                             connType, argList->at(0), argList->at(1), argList->at(2), argList->at(3), argList->at(4), argList->at(5));
                             break;
                         case 7:
                             this->metaObject()->invokeMethod(obj, signal_to_emit.toAscii(), \
-                             Qt::DirectConnection, argList->at(0), argList->at(1), argList->at(2), argList->at(3), argList->at(4), \
+                             connType, argList->at(0), argList->at(1), argList->at(2), argList->at(3), argList->at(4), \
                              argList->at(5), argList->at(6));
                             break;
                         case 8:
                             this->metaObject()->invokeMethod(obj, signal_to_emit.toAscii(), \
-                             Qt::DirectConnection, argList->at(0), argList->at(1), argList->at(2), argList->at(3), argList->at(4), \
+                             connType, argList->at(0), argList->at(1), argList->at(2), argList->at(3), argList->at(4), \
                              argList->at(5), argList->at(6), argList->at(7));
                             break;
                         case 9:
                             this->metaObject()->invokeMethod(obj, signal_to_emit.toAscii(), \
-                             Qt::DirectConnection, argList->at(0), argList->at(1), argList->at(2), argList->at(3), argList->at(4), \
+                             connType, argList->at(0), argList->at(1), argList->at(2), argList->at(3), argList->at(4), \
                              argList->at(5), argList->at(6), argList->at(7), argList->at(8));
                             break;
                         case 10:
                             this->metaObject()->invokeMethod(obj, signal_to_emit.toAscii(), \
-                             Qt::DirectConnection, argList->at(0), argList->at(1), argList->at(2), argList->at(3), argList->at(4), \
+                             connType, argList->at(0), argList->at(1), argList->at(2), argList->at(3), argList->at(4), \
                              argList->at(5), argList->at(6), argList->at(7), argList->at(8), argList->at(9));
                             break;
                         default: {
@@ -102,49 +114,49 @@ void mafEventDispatcherLocal::notifyEvent(const mafEvent &event_dictionary, mafE
                  } else { //use return value
                     switch (argList->count()) {
                         case 0:
-                            this->metaObject()->invokeMethod(obj, signal_to_emit.toAscii(), Qt::DirectConnection, *returnArg);
+                            this->metaObject()->invokeMethod(obj, signal_to_emit.toAscii(), connType, *returnArg);
                             break;
                         case 1:
-                            this->metaObject()->invokeMethod(obj, signal_to_emit.toAscii(), Qt::DirectConnection,\
+                            this->metaObject()->invokeMethod(obj, signal_to_emit.toAscii(), connType,\
                             *returnArg, argList->at(0));
                             break;
                         case 2:
-                            this->metaObject()->invokeMethod(obj, signal_to_emit.toAscii(), Qt::DirectConnection,\
+                            this->metaObject()->invokeMethod(obj, signal_to_emit.toAscii(), connType,\
                              *returnArg, argList->at(0), argList->at(1));
                             break;
                         case 3:
-                            this->metaObject()->invokeMethod(obj, signal_to_emit.toAscii(), Qt::DirectConnection,\
+                            this->metaObject()->invokeMethod(obj, signal_to_emit.toAscii(), connType,\
                             *returnArg, argList->at(0), argList->at(1), argList->at(2));
                             break;
                         case 4:
-                            this->metaObject()->invokeMethod(obj, signal_to_emit.toAscii(), Qt::DirectConnection,\
+                            this->metaObject()->invokeMethod(obj, signal_to_emit.toAscii(), connType,\
                              *returnArg, argList->at(0), argList->at(1), argList->at(2), argList->at(3));
                             break;
                         case 5:
-                            this->metaObject()->invokeMethod(obj, signal_to_emit.toAscii(), Qt::DirectConnection,\
+                            this->metaObject()->invokeMethod(obj, signal_to_emit.toAscii(), connType,\
                             *returnArg, argList->at(0), argList->at(1), argList->at(2), argList->at(3), argList->at(4));
                             break;
                         case 6:
-                            this->metaObject()->invokeMethod(obj, signal_to_emit.toAscii(), Qt::DirectConnection,\
+                            this->metaObject()->invokeMethod(obj, signal_to_emit.toAscii(), connType,\
                             *returnArg, argList->at(0), argList->at(1), argList->at(2), argList->at(3), argList->at(4), argList->at(5));
                             break;
                         case 7:
-                            this->metaObject()->invokeMethod(obj, signal_to_emit.toAscii(), Qt::DirectConnection,\
+                            this->metaObject()->invokeMethod(obj, signal_to_emit.toAscii(), connType,\
                             *returnArg, argList->at(0), argList->at(1), argList->at(2), argList->at(3), argList->at(4), \
                              argList->at(5), argList->at(6));
                             break;
                         case 8:
-                            this->metaObject()->invokeMethod(obj, signal_to_emit.toAscii(), Qt::DirectConnection,\
+                            this->metaObject()->invokeMethod(obj, signal_to_emit.toAscii(), connType,\
                              *returnArg, argList->at(0), argList->at(1), argList->at(2), argList->at(3), argList->at(4), \
                              argList->at(5), argList->at(6), argList->at(7));
                             break;
                         case 9:
-                            this->metaObject()->invokeMethod(obj, signal_to_emit.toAscii(), Qt::DirectConnection,\
+                            this->metaObject()->invokeMethod(obj, signal_to_emit.toAscii(), connType,\
                             *returnArg, argList->at(0), argList->at(1), argList->at(2), argList->at(3), argList->at(4), \
                              argList->at(5), argList->at(6), argList->at(7), argList->at(8));
                             break;
                         case 10:
-                            this->metaObject()->invokeMethod(obj, signal_to_emit.toAscii(), Qt::DirectConnection,\
+                            this->metaObject()->invokeMethod(obj, signal_to_emit.toAscii(), connType,\
                             *returnArg, argList->at(0), argList->at(1), argList->at(2), argList->at(3), argList->at(4), \
                             argList->at(5), argList->at(6), argList->at(7), argList->at(8), argList->at(9));
                             break;
@@ -156,9 +168,9 @@ void mafEventDispatcherLocal::notifyEvent(const mafEvent &event_dictionary, mafE
                  }
             } else {
                 if (returnArg == NULL || returnArg->data() == NULL) { //don't use return value
-                    this->metaObject()->invokeMethod(obj, signal_to_emit.toAscii(), Qt::DirectConnection);
+                    this->metaObject()->invokeMethod(obj, signal_to_emit.toAscii(), connType);
                 } else {
-                    this->metaObject()->invokeMethod(obj, signal_to_emit.toAscii(), Qt::DirectConnection, *returnArg);
+                    this->metaObject()->invokeMethod(obj, signal_to_emit.toAscii(), connType, *returnArg);
                 }
             }
         }
