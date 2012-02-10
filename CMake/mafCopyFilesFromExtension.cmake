70fae06e0172f80feaf747dcbf59767683ea0560
Daniele Giunchi <d.giunchi@scsitaly.com>
2012-02-10 16:19:24 +0100

    change project variable name to project name

--- CMake/mafCopyFilesFromExtension.cmake
+++ CMake/mafCopyFilesFromExtension.cmake
@@ -8,18 +8,18 @@
 #  See Licence at: http://tiny.cc/QXJ4D
 #
 #
-MACRO(mafCopyFilesFromExtension extension project)
+MACRO(mafCopyFilesFromExtension extension project_name)
 if(${BUILD_SHARED_LIBS})
 set(TARGET_LOC)
 set(DEST_ABSOLUTE_DIR)
-get_target_property(TARGET_LOC ${project} LOCATION)
+get_target_property(TARGET_LOC ${project_name} LOCATION)
 get_filename_component(DEST_ABSOLUTE_DIR ${TARGET_LOC} PATH)
 
 FILE(GLOB tocopy_file_list "${PROJECT_SOURCE_DIR}/*.${extension}")
 
 foreach(file ${tocopy_file_list})
   get_filename_component(fileName ${file} NAME)
-  add_custom_command(TARGET ${project} POST_BUILD
+  add_custom_command(TARGET ${project_name} POST_BUILD
         COMMAND ${CMAKE_COMMAND} -E copy ${file} ${DEST_ABSOLUTE_DIR}/${fileName} )
 endforeach(file ${tocopy_file_list})
 endif(${BUILD_SHARED_LIBS})
