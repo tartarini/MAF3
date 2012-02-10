8af12240f5fe3627b5a346c6b713bbd0100d2f8e
Paolo Quadrani <paolo.quadrani@gmail.com>
2012-02-10 12:39:58 +0100

    Fixed license information.

--- src/mafCore/mafObjectRegistry.h
+++ src/mafCore/mafObjectRegistry.h
@@ -3,9 +3,9 @@
  *  mafCore
  *
  *  Created by Paolo Quadrani on 27/03/09.
- *  Copyright 2009 B3C. All rights reserved.
+ *  Copyright 2012 B3C. All rights reserved.
  *
- *  See Licence at: http://tiny.cc/QXJ4D
+ *  See License at: http://tiny.cc/QXJ4D
  *
  */
 
