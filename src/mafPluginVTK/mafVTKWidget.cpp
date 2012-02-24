18e0130105a90398b85855a754dd1c7f79527701
Daniele Giunchi <d.giunchi@scsitaly.com>
2012-02-24 15:51:07 +0100

    set clipping range camera

--- src/mafPluginVTK/mafVTKWidget.cpp
+++ src/mafPluginVTK/mafVTKWidget.cpp
@@ -111,7 +111,9 @@ vtkRenderer *mafVTKWidget::createLayer(const QString layerName) {
         renderer->TwoSidedLightingOn();
         // Link the camera to that one present into the base renderer (if available)
         if (m_RendererBase != NULL) {
-            renderer->SetActiveCamera(m_RendererBase->GetActiveCamera());
+            vtkCamera *camera = m_RendererBase->GetActiveCamera();
+            camera->SetClippingRange(0.1, 1000);
+            renderer->SetActiveCamera(camera);
         }
         // Add the new renderer to the render window
         renWin->AddRenderer(renderer);
