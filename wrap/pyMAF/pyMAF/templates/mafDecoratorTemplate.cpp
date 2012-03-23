3d71d1f858afe2a55f45fcfca283124ce06a2b0b
Daniele Giunchi <d.giunchi@scsitaly.com>
2012-03-23 16:08:14 +0100

    add property accessors

--- wrap/pyMAF/pyMAF/templates/mafDecoratorTemplate.cpp
+++ wrap/pyMAF/pyMAF/templates/mafDecoratorTemplate.cpp
@@ -23,4 +23,13 @@ public Q_SLOTS:
   void delete_{{compoundname}}({{compoundname}}* selfObj) {
     {% if public_destructor %} delete selfObj;{% else %}selfObj->{{destructor_method}}();{%endif%}
   }
+  
+  QVariant property({{compoundname}}* selfObj, const char *name) {
+    return selfObj->property(name);
+  }
+  
+  void setProperty({{compoundname}}* selfObj, const char *name, QVariant value) {
+    return selfObj->setProperty(name, value);
+  }
+  
 };
