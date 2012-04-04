799c6fa2486cd6aa3b6318693aa18236ca6716e7
Daniele Giunchi <d.giunchi@scsitaly.com>
2012-04-04 16:32:45 +0200

    remove duplicate find package

--- CMakeExternals/Find_PythonQt.cmake
+++ CMakeExternals/Find_PythonQt.cmake
@@ -6,10 +6,6 @@
 SET(PYTHONQT_DIR "${MAF_EXTERNAL_BUILD_DIR}/Build/PythonQt")
 SET(PYTHONQT_SOURCE_DIR "${MAF_EXTERNAL_BUILD_DIR}/Source/PythonQt")
 # Python is required
-FIND_PACKAGE(PythonLibs)
-IF(NOT PYTHONLIBS_FOUND)
-  MESSAGE(FATAL_ERROR "error: Python is required to build PythonQt")
-ENDIF()
 
 include_directories("${PYTHONQT_SOURCE_DIR}/src")
 link_directories(${PYTHONQT_DIR})
