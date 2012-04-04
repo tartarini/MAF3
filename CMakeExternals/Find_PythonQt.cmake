0185dbfbefcd6b2932e5fd427fa9c9234636cc51
Daniele Giunchi <d.giunchi@scsitaly.com>
2012-04-04 22:12:32 +0200

    add python dependency

--- CMakeExternals/Find_PythonQt.cmake
+++ CMakeExternals/Find_PythonQt.cmake
@@ -6,6 +6,11 @@
 SET(PYTHONQT_DIR "${MAF_EXTERNAL_BUILD_DIR}/Build/PythonQt")
 SET(PYTHONQT_SOURCE_DIR "${MAF_EXTERNAL_BUILD_DIR}/Source/PythonQt")
 # Python is required
+FIND_PACKAGE(PythonLibs)
+FIND_PACKAGE(PythonInterp)
+IF(NOT PYTHONLIBS_FOUND)
+  MESSAGE(FATAL_ERROR "error: Python is required to build ${PROJECT_NAME}")
+ENDIF()
 
 include_directories("${PYTHONQT_SOURCE_DIR}/src")
 link_directories(${PYTHONQT_DIR})
