b525039b61870ea6aed22048f5e9e0295f3cca03
Paolo Quadrani <paolo.quadrani@gmail.com>
2012-04-16 13:30:13 +0200

    Fixed misleading Critical message during plug-in loading and fixed text messages.

--- src/mafResources/mafPlugin.cpp
+++ src/mafResources/mafPlugin.cpp
@@ -18,25 +18,23 @@ using namespace mafResources;
 mafPlugin::mafPlugin(const QString &pluginFilename, const QString code_location) : mafPluginInterface(pluginFilename, code_location), m_LibraryHandler(0),
                                                           m_PluginInfo(0),
                                                           m_RegisterPlugin(0) {
-    // Try to load the plugin as a dynamic library
+    // Try to load the Plug-in as a dynamic library
     m_LibraryHandler = new QLibrary(pluginFilename);
     if(!m_LibraryHandler->load()) {
-        QString err_msg(mafTr("Could not load '%1'").arg(pluginFilename));
-        qCritical() << err_msg;
         delete m_LibraryHandler;
         m_LibraryHandler = NULL;
         return;
     }
 
-    // Locate the plugin's exported functions
+    // Locate the Plug-ins exported functions
     try {
         m_PluginInfo = reinterpret_cast<mafFnPluginInfo *>(m_LibraryHandler->resolve("pluginInfo"));
         m_RegisterPlugin = reinterpret_cast<mafFnRegisterPlugin *>(m_LibraryHandler->resolve("registerPlugin"));
 
         // If the functions aren't found, we're going to assume this is
-        // a plain simple DLL and not one of our plugins
+        // a plain simple DLL and not one of our Plug-ins
         if(!m_PluginInfo || !m_RegisterPlugin) {
-            QString err_msg(mafTr("'%1' is not a valid MAF3 plugin").arg(pluginFilename));
+            QString err_msg(mafTr("'%1' is not a valid MAF3 Plug-in").arg(pluginFilename));
             qCritical() << err_msg;
             delete m_LibraryHandler;
             m_LibraryHandler = NULL;
@@ -46,7 +44,7 @@ mafPlugin::mafPlugin(const QString &pluginFilename, const QString code_location)
         // Initialize a new DLL reference counter
         m_RefCount = new size_t(1);
     } catch(...) {
-        QString err_msg(mafTr("Unknoun error occourred loading plugin '%1'").arg(pluginFilename));
+        QString err_msg(mafTr("Unknown error occurred loading Plug-in '%1'").arg(pluginFilename));
         qCritical() << err_msg;
         m_LibraryHandler->unload();
         delete m_LibraryHandler;
@@ -64,7 +62,7 @@ mafPlugin::mafPlugin(const mafPlugin &Other, const QString code_location) : mafP
 
 mafPlugin::~mafPlugin() {
     // Only unload the DLL if there are no more references to it
-    // Check also if m_RefCount is not NULL (it could be NULL when plugin loading error occourr).
+    // Check also if m_RefCount is not NULL (it could be NULL when Plug-in loading error occur).
     if(m_RefCount && !--*m_RefCount) {
         delete m_RefCount;
         //m_LibraryHandler->unload();
