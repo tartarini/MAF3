fb62672ecee689b5ed1870cb722d7be0accd667b
Paolo Quadrani <paolo.quadrani@gmail.com>
2012-04-16 13:31:24 +0200

    Fixed license information.

--- src/mafResources/mafPlugin.cpp
+++ src/mafResources/mafPlugin.cpp
@@ -3,9 +3,9 @@
  *  mafResources
  *
  *  Created by Paolo Quadrani on 30/12/09.
- *  Copyright 2009 B3C. All rights reserved.
+ *  Copyright 2012 B3C. All rights reserved.
  *
- *  See Licence at: http://tiny.cc/QXJ4D
+ *  See License at: http://tiny.cc/QXJ4D
  *
  */
 
