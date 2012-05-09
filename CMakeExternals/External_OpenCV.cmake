176cd33348e240eead98a7cc75e7a91cc6597bef
Daniele Giunchi <d.giunchi@scsitaly.com>
2012-05-09 15:05:32 +0200

    remove the check connection condition

--- CMakeExternals/External_OpenCV.cmake
+++ CMakeExternals/External_OpenCV.cmake
@@ -10,9 +10,9 @@ SET(OpenCV_DEPENDS)
   SET(proj_DEPENDENCIES)
   
   SET(OpenCV_DEPENDS ${proj})
-  execute_process(COMMAND ${GIT_EXECUTABLE} remote show "${git_protocol}://github.com/b3c/OpenCV.git"
-                  WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
-                  RESULT_VARIABLE RESULT)
+  #execute_process(COMMAND ${GIT_EXECUTABLE} remote show "${git_protocol}://github.com/b3c/OpenCV.git"
+  #                WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+  #                RESULT_VARIABLE RESULT)
   
   SET(ep_base "${MAF_EXTERNAL_BUILD_DIR}")
   SET_PROPERTY(DIRECTORY PROPERTY EP_BASE ${ep_base})
@@ -29,40 +29,40 @@ if(APPLE)
   set(CMAKE_XCODE_ATTRIBUTE_GCC_VERSION "com.apple.compilers.llvmgcc42")
 endif(APPLE)
 
-IF(RESULT)
-  IF(NOT DEFINED OpenCV_DIR)
+#IF(RESULT)
+#  IF(NOT DEFINED OpenCV_DIR)
     #MESSAGE("Adding project:${proj}")
     
-    ExternalProject_Add(${proj}
-      DOWNLOAD_DIR "${ep_build_dir}/${proj}"
-      CMAKE_GENERATOR ${gen}
-      INSTALL_COMMAND ""
-      CMAKE_ARGS
-        ${ep_common_args}
-        -DCMAKE_XCODE_ATTRIBUTE_GCC_VERSION=${CMAKE_XCODE_ATTRIBUTE_GCC_VERSION}
-        -DBUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS}
-        -DWITH_FFMPEG:BOOL=FALSE
-        -DBUILD_NEW_PYTHON_SUPPORT:BOOL=FALSE
-        -DBUILD_DOXYGEN_DOCS:BOOL=FALSE
-        -DBUILD_DOXYGEN_EXAMPLES:BOOL=FALSE
-        -DBUILD_DOXYGEN_TESTS:BOOL=FALSE
-        -DBUILD_DOXYGEN_PACKAGE:BOOL=FALSE
-        -DBUILD_DOXYGEN_WITH_CUDA:BOOL=FALSE
-        -DQT_QMAKE_EXECUTABLE:FILEPATH=${QT_QMAKE_EXECUTABLE}
-      DEPENDS
-        ${proj_DEPENDENCIES}
-      )
-    SET(OpenCV_DIR ${ep_build_dir}/${proj})
-    
-    # Since OpenCV is statically build, there is not need to add its corresponding 
-    # library output directory to CTK_EXTERNAL_LIBRARY_DIRS
+#    ExternalProject_Add(${proj}
+#      DOWNLOAD_DIR "${ep_build_dir}/${proj}"
+#      CMAKE_GENERATOR ${gen}
+#      INSTALL_COMMAND ""
+#      CMAKE_ARGS
+#        ${ep_common_args}
+#        -DCMAKE_XCODE_ATTRIBUTE_GCC_VERSION=${CMAKE_XCODE_ATTRIBUTE_GCC_VERSION}
+#        -DBUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS}
+#        -DWITH_FFMPEG:BOOL=FALSE
+#        -DBUILD_NEW_PYTHON_SUPPORT:BOOL=FALSE
+#        -DBUILD_DOXYGEN_DOCS:BOOL=FALSE
+#        -DBUILD_DOXYGEN_EXAMPLES:BOOL=FALSE
+#        -DBUILD_DOXYGEN_TESTS:BOOL=FALSE
+#        -DBUILD_DOXYGEN_PACKAGE:BOOL=FALSE
+#        -DBUILD_DOXYGEN_WITH_CUDA:BOOL=FALSE
+#        -DQT_QMAKE_EXECUTABLE:FILEPATH=${QT_QMAKE_EXECUTABLE}
+#      DEPENDS
+#        ${proj_DEPENDENCIES}
+#      )
+#    SET(OpenCV_DIR ${ep_build_dir}/${proj})
+#    
+#    # Since OpenCV is statically build, there is not need to add its corresponding 
+#    # library output directory to CTK_EXTERNAL_LIBRARY_DIRS
   
-  ELSE()
-    include(CMake/mafMacroEmptyExternalProject.cmake)
-    mafMacroEmptyExternalProject(${proj} "${proj_DEPENDENCIES}")
-  ENDIF()
+#  ELSE()
+#    include(CMake/mafMacroEmptyExternalProject.cmake)
+#    mafMacroEmptyExternalProject(${proj} "${proj_DEPENDENCIES}")
+#  ENDIF()
 
-ELSE(RESULT)
+#ELSE(RESULT)
   IF(NOT DEFINED OpenCV_DIR)
     #MESSAGE("Adding project:${proj}")
     
@@ -97,7 +97,7 @@ ELSE(RESULT)
     mafMacroEmptyExternalProject(${proj} "${proj_DEPENDENCIES}")
   ENDIF()
 
-ENDIF(RESULT)
+#ENDIF(RESULT)
   SET(${OpenCV_enabling_variable}_INCLUDE_DIRS OpenCV_INCLUDE_DIRS)
   SET(${OpenCV_enabling_variable}_FIND_PACKAGE_CMD OpenCV)
     
