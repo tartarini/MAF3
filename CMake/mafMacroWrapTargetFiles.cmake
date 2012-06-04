9145ffb54e231ec40637452eb80393f6b21c0a15
mgiacomoni <m.giacomoni@scsitaly.com>
2012-06-04 15:07:03 +0200

    Fix cmake configuration on windows

--- CMake/mafMacroWrapTargetFiles.cmake
+++ CMake/mafMacroWrapTargetFiles.cmake
@@ -11,9 +11,11 @@
 
 MACRO(mafMacroWrapTargetFiles) 
 
-find_package(PythonInterp)
-execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${MAF_ROOT_BINARY_DIR}/wrap/mafPythonQtDecorators/${PROJECT_NAME})
-execute_process(COMMAND ${PYTHON_EXECUTABLE} ${MAF_ROOT_BINARY_DIR}/wrap/pyMAF/mafPythonWrapper.py ${MAF_SOURCE_DIR}/src/${PROJECT_NAME}/target_wrap_files ${MAF_ROOT_BINARY_DIR}/doc/DocOutput/xml ${MAF_ROOT_BINARY_DIR}/wrap/mafPythonQtDecorators/${PROJECT_NAME})
+IF(BUILD_WRAP)
+	find_package(PythonInterp)
+	execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${MAF_ROOT_BINARY_DIR}/wrap/mafPythonQtDecorators/${PROJECT_NAME})
+	execute_process(COMMAND ${PYTHON_EXECUTABLE} ${MAF_ROOT_BINARY_DIR}/wrap/pyMAF/mafPythonWrapper.py ${MAF_SOURCE_DIR}/src/${PROJECT_NAME}/target_wrap_files ${MAF_ROOT_BINARY_DIR}/doc/DocOutput/xml ${MAF_ROOT_BINARY_DIR}/wrap/mafPythonQtDecorators/${PROJECT_NAME})
+	ENDIF(BUILD_WRAP)
 ENDMACRO()
 
 MACRO(mafMacroWrapBuild)
