9f6e4eea31800b18ab97dbb4d0ae67efde26fdb0
Daniele Giunchi <d.giunchi@scsitaly.com>
2012-08-30 16:01:24 +0200

    fix problem when pull from data repository

--- CMake/mafDataTestDownload.cmake
+++ CMake/mafDataTestDownload.cmake
@@ -1,11 +1,21 @@
 MACRO (mafDataTestDownload)
   set(GIT_REPOSITORY "git://github.com/b3c/maf3data.git")
 
+  if(NOT EXISTS ${MAF_DATA_DIR})
   execute_process(
     COMMAND "${GIT_EXECUTABLE}" clone ${GIT_REPOSITORY} ${MAF_DATA_DIR}
     OUTPUT_VARIABLE _out
     ERROR_VARIABLE _err
     RESULT_VARIABLE _res)
+  else(NOT EXISTS ${MAF_DATA_DIR})
+    execute_process(
+    WORKING_DIRECTORY ${MAF_DATA_DIR}
+    COMMAND "${GIT_EXECUTABLE}" pull 
+    OUTPUT_VARIABLE _out
+    ERROR_VARIABLE _err
+    RESULT_VARIABLE _res)
+  endif(NOT EXISTS ${MAF_DATA_DIR})
+  
 
   message(STATUS "out: ${_out}")
   message(STATUS "err: ${_err}")
