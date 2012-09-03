52f1121432cb99b46867a9f81c4bf6d7b96863c3
Daniele Giunchi <d.giunchi@scsitaly.com>
2012-09-03 09:28:31 +0200

    enable heapcheck

--- src/mafQA/qa/BinaryLauncher.py
+++ src/mafQA/qa/BinaryLauncher.py
@@ -93,8 +93,8 @@ def execute():
             if(str(os.sys.platform).lower() == 'win32'):
                 os.system(absPath + " > " + logResult + " 2>&1 ")
             else:
-                #os.system("HEAPCHECK=local " + absPath + " &> " + logResult)
-                os.system(absPath + " &> " + logResult)
+                os.system("HEAPCHECK=local " + absPath + " &> " + logResult)
+                #os.system(absPath + " &> " + logResult)
             name_key = "test-suite"
             if name_key in param:
                 validate(logResult)
