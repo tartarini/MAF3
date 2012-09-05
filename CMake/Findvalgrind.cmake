69db23b45f21ab06be68ad69c73933d9e77abb61
Daniele Giunchi <d.giunchi@scsitaly.com>
2012-09-05 15:43:38 +0200

    check correct path for valgrind

--- CMake/Findvalgrind.cmake
+++ CMake/Findvalgrind.cmake
@@ -5,7 +5,7 @@
 SET(valgrind_FOUND 0)
 find_program(valgrind_LIBRARY 
               NAMES valgrind 
-              PATHS /usr/local/bin /usr/bin)
+              PATHS /usr/local/bin /usr/bin ${valgrind_LIBRARY})
 
 get_filename_component(valgrind_LIBRARY_DIR ${valgrind_LIBRARY} PATH)
 
