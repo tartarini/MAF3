077b9377b4ddfea39ffa9a4109ca4f62df5a57b4
Daniele Giunchi <d.giunchi@scsitaly.com>
2012-09-06 11:10:55 +0200

    fix bug

--- CMake/Findvalgrind.cmake
+++ CMake/Findvalgrind.cmake
@@ -6,10 +6,10 @@ SET(valgrind_FOUND 0)
 
 find_program(valgrind_EXECUTABLE 
               NAMES valgrind 
-              PATHS /usr/local/bin /usr/bin)
+              PATHS /usr/local/bin /usr/bin ${valgrind_PATH})
 
 #fill the directory
-get_filename_component(valgrind_LIBRARY_DIR ${valgrind_EXECUTABLE} PATH)
+get_filename_component(valgrind_PATH ${valgrind_EXECUTABLE} PATH)
 
 MARK_AS_ADVANCED(valgrind_FOUND)
  
